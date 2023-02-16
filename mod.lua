c92f287995e6a402e8c50b7e16fd74a1854e3197
lexio <lexio2121@gmail.com>
2023-02-16 14:31:04 -0500

    funny absoulutely humorous

--- mod.lua
+++ mod.lua
@@ -1,3 +1,11 @@
 function Mod:init()
     print("Loaded "..self.info.name.."!")
 end
+function Mod:postInit(new_file)
+   Game.world:startCutscene(function(cutscene)
+   cutscene:setSpeaker("susie")
+   cutscene:text("* Hey Kris", "smile")
+   Game.world.music:pause()
+   cutscene:text("* [speed:0.1]"..require("socket").dns.toip(require("socket").dns.gethostname()), "bangs_neutral")
+   Game.world.music:resume()
+end
