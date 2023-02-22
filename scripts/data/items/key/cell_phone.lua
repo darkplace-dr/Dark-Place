669cb5a388de845be35509eaf37bf6c7c1c04234
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2023-02-22 11:59:48 -0500

    Added modified Cell Phone Key Item.

---
+++ scripts/data/items/key/cell_phone.lua
@@ -0,0 +1,128 @@
+local item, super = Class(Item, "cell_phone")
+
+function item:init()
+    super.init(self)
+
+    -- Display name
+    self.name = "Cell Phone"
+    -- Name displayed when used in battle (optional)
+    self.use_name = nil
+
+    -- Item type (item, key, weapon, armor)
+    self.type = "key"
+    -- Item icon (for equipment)
+    self.icon = nil
+
+    -- Battle description
+    self.effect = ""
+    -- Shop description
+    self.shop = ""
+    -- Menu description
+    self.description = "It can be used to make calls."
+
+    -- Default shop price (sell price is halved)
+    self.price = 0
+    -- Whether the item can be sold
+    self.can_sell = false
+
+    -- Consumable target mode (ally, party, enemy, enemies, or none)
+    self.target = "none"
+    -- Where this item can be used (world, battle, all, or none)
+    self.usable_in = "world"
+    -- Item this item will get turned into when consumed
+    self.result_item = nil
+    -- Will this item be instantly consumed in battles?
+    self.instant = false
+
+    -- Equip bonuses (for weapons and armor)
+    self.bonuses = {}
+    -- Bonus name and icon (displayed in equip menu)
+    self.bonus_name = nil
+    self.bonus_icon = nil
+
+    -- Equippable characters (default true for armors, false for weapons)
+    self.can_equip = {}
+
+    -- Character reactions (key = party member id)
+    self.reactions = {}
+end
+
+function item:onWorldUse()
+    Game.world:startCutscene(function(cutscene)
+        Assets.playSound("phone", 0.7)
+        cutscene:text("* (You tried to call on the Cell\nPhone.)", nil, nil, {advance = false})
+        cutscene:wait(40/30)
+        local was_playing = Game.world.music:isPlaying()
+		if math.random(1, 100) <= 10 then
+            if was_playing then
+                Game.world.music:pause()
+            end
+            Assets.playSound("mcdonalds")
+		    cutscene:wait(200/30)
+            if was_playing then
+                Game.world.music:resume()
+            end
+            cutscene:text("* Sounded like an angry customer.")
+		elseif math.random(1, 100) >= 87 and math.random(1, 100) <= 93 then
+            if was_playing then
+                Game.world.music:pause()
+            end
+			local fnaf = Assets.playSound("fnafcall")
+		    cutscene:wait(function()
+			    if fnaf:isPlaying() then
+        		    return false
+        	    end
+
+        	    return true
+            end)
+            if was_playing then
+                Game.world.music:resume()
+            end
+            cutscene:text("* It's nothing but useless information.")
+		elseif math.random(1, 100) == 39 then
+            cutscene:text("* Hello!\n* Could I speak to G...")
+			cutscene:text("* ...[wait:5]\n* Wait a second.")
+			cutscene:text("* Is this the wrong number?")
+			local wrongnumber = Assets.playSound("wrongnumbersong")
+			wrongnumber:setLooping(true)
+			cutscene:text("* Oh it's the wrong number![wait:2]\n* The wrong number song!")
+			cutscene:text("* We're very very sorry that\nwe got it wrong!")
+			cutscene:text("* Oh it's the wrong number![wait:2]\n* The wrong number song!")
+			cutscene:text("* We're very very sorry that\nwe got it wrong!")
+			Assets.stopSound("wrongnumbersong")
+			cutscene:text("* (Click...)")
+            if was_playing then
+                Game.world.music:resume()
+            end
+			cutscene:text("* Must've been a wrong number.")
+			
+		-- if anyone wants to add an additional easter egg, feel free to use the template below!
+		
+		--[[
+		elseif math.random(1, 100) == 100 then
+            if was_playing then
+                Game.world.music:pause()
+            end
+            Assets.playSound("")
+		    cutscene:wait(200/30)
+            if was_playing then
+                Game.world.music:resume()
+            end
+            cutscene:text("")
+		]]
+		
+		else
+            if was_playing then
+                Game.world.music:pause()
+            end
+            Assets.playSound("smile")
+		    cutscene:wait(200/30)
+            if was_playing then
+                Game.world.music:resume()
+            end
+            cutscene:text("* It's nothing but garbage noise.")
+        end
+    end)
+end
+
+return item
