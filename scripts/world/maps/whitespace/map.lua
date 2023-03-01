668fc891a89dcacd60574335912bc7ebb949509c
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2023-02-28 23:12:38 -0500

    Updated map.lua for White Space map.

--- scripts/world/maps/whitespace/map.lua
+++ scripts/world/maps/whitespace/map.lua
@@ -7,17 +7,17 @@ end
 function WhiteSpace:update()
     if Game.world.player.x <= 360 then
         Game.world.player.x = Game.world.player.x + 5060
-        Game.world.followers = {}
+        Game.world.player:interpolateFollowers()
     elseif Game.world.player.x >= 5420 then
         Game.world.player.x = Game.world.player.x - 5060
-        Game.world.followers = {}
+        Game.world.player:interpolateFollowers()
     end
     if Game.world.player.y <= 280 then
         Game.world.player.y = Game.world.player.y + 3780
-        Game.world.followers = {}
+        Game.world.player:interpolateFollowers()
     elseif Game.world.player.y >= 4060 then
         Game.world.player.y = Game.world.player.y - 3780
-        Game.world.followers = {}
+        Game.world.player:interpolateFollowers()
 	end
     super:update(self)
 end
