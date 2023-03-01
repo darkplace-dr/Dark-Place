567d1d3fa73103c7079c2540a567c42bf5a78091
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2023-02-28 22:40:44 -0500

    Added WHITE SPACE lightbulb cutscene.

--- scripts/world/cutscenes/whitespace.lua
+++ scripts/world/cutscenes/whitespace.lua
@@ -13,7 +13,7 @@ return {
     end,
     sketchbook = function(cutscene, event)
         cutscene:text("* A sketchbook full of strange-looking drawings.")
-        cutscene:text("* There appears to be a crude-looking disembodied head drawn on the cover.")
+        cutscene:text("* There appears to be a crude, disembodied head drawn on the cover.")
     end,
     lightbulb = function(cutscene, event)
         cutscene:text("* A lightbulb hangs from the ceiling,[wait:5] wherever it is.")
@@ -22,7 +22,28 @@ return {
         choice = cutscene:choicer({"Yes", "No"}, options)
 
         if choice == 1 then
+            cutscene:wait(0.2)
+            cutscene:fadeOut(0.5)
+            cutscene:wait(0.5)
+			
+            local stare = Sprite("objects/lightbulb_stare")
+            stare:play(0.33, true)
+			stare.layer = 500
+            stare.parallax_x = 0
+            stare.parallax_y = 0
+            Game.world:addChild(stare)
+
+            cutscene:wait(0.5)
+            cutscene:fadeIn(0.5)
+            cutscene:wait(2)
+
             cutscene:text("* It's pitch black inside.\n[wait:10]* You can't see a thing.")
+			cutscene:wait(0.5)
+            cutscene:fadeOut(0.5)
+            cutscene:wait(0.5)
+			stare:remove()
+            cutscene:fadeIn(0.5)
+            cutscene:wait(0.5)
         end
     end,
 }
