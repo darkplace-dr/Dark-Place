12e09b20b4eba6e27ddb8466fc0539b9d391e4fd
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2023-03-15 23:37:27 -0400

    Update touhoubullet.lua

--- scripts/battle/bullets/poseur/touhoubullet.lua
+++ scripts/battle/bullets/poseur/touhoubullet.lua
@@ -5,27 +5,33 @@ function TouhouBullet:init(x, y, dir, speed)
     super.init(self, x, y, "bullets/poseurbullet")
     self:setScale(1, 1)
 
-    -- Move the bullet in dir radians (0 = right, pi = left, clockwise rotation)
-    self.physics.direction = dir
-    -- Speed the bullet moves (pixels per frame at 30FPS)
-    self.physics.speed = speed
     self.destroy_on_hit = false
     self.remove_offscreen = false
 
     self.timer = 0
     self.offset = 0
+	
     self.negmult = 0
-    self.neg = 1
+	self.lerp = 0
 end
 
-function TouhouBullet:update()
+function TouhouBullet:update(dt)
+    self.timer = self.timer
+    self.offset = self.offset
+    self.lerp = self.lerp
+    self.neg = 1
     -- For more complicated bullet behaviours, code here gets called every update
-    local posx = math.cos(self.timer + self.offset) + 1
-    local posy = math.sin(self.timer*self.negmult + self.offset)
-    self:move(-posx, posy)
+    local posx = (70 * self.lerp) * math.sin(self.timer * self.negmult + self.offset)
+    local posy = (70 * self.lerp) * math.cos(self.timer + self.offset) + self.lerp * 50
+    self:move(posx, posy)
     self.timer = self.timer + 1/40
 
-    super.update(self)
+    self.lerp = self.lerp + 1/90
+	if self.lerp > 4 then
+        self.lerp = 4
+	end
+
+    super:update(self, dt)
 end
 
 return TouhouBullet
