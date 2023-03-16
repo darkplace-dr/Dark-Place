56bdfc6237c89bd0af8d675eb4d42fc60cc86e60
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2023-03-15 23:36:45 -0400

    Update touhou.lua

--- scripts/battle/waves/poseur/touhou.lua
+++ scripts/battle/waves/poseur/touhou.lua
@@ -2,12 +2,15 @@ local Touhou, super = Class(Wave)
 
 function Touhou:onStart()
     local mult = 0.5
+	local yOffset = 80
     self.timer:every(1/2, function()
         local numbullets = 10
-        for i=1,numbullets+1 do
-            local bullet = self:spawnBullet("poseur/touhoubullet", Game.battle.arena.right + 80, (Game.battle.arena.top + Game.battle.arena.bottom) / 2)
+        for i = 1, numbullets do
+            local bullet = self:spawnBullet("poseur/touhoubullet", SCREEN_WIDTH / 2, yOffset)
+			bullet.timer = 0
             bullet.offset = math.pi * 2 * i / numbullets
             bullet.negmult = mult
+			bullet.lerp = 0
             mult = mult + 0.05
         end
     end)
