d6c0edfeef713a519b169cd58ef237d3a6bdc6e1
Liu Wenyuan <15816141883@163.com>
2023-04-01 21:06:25 +0800

    Line flash

--- scripts/objects/world/WarpBinInputMenu.lua
+++ scripts/objects/world/WarpBinInputMenu.lua
@@ -12,13 +12,15 @@ function WarpBinInputMenu:init()
     self.box.debug_select = false
     self:addChild(self.box)
 
+    self.caret_flash_timer = 0
+
     self.font = Assets.getFont("main_mono", 32)
     self.char_w = 32
     self.char_h = self.char_w
     self.char_spacing = 5
 
     -- a table of lines
-	self.input = {""}
+    self.input = {""}
     self.code_len = 8
 
     TextInput.attachInput(self.input, {
@@ -29,6 +31,7 @@ function WarpBinInputMenu:init()
         -- Limit the Bin's Digits
         -- only uppercase letters
         self.input[1] = string.upper(string.sub(self.input[1], 1, 8))
+        self.caret_flash_timer = 0
     end
     TextInput.submit_callback = function()
         if self.finish_cb then
@@ -43,18 +46,26 @@ end
 function WarpBinInputMenu:draw()
     love.graphics.setColor(1, 1, 1)
     love.graphics.setFont(self.font)
+
     local draw_x = 0
     local draw_y = 0
     for i = 1, self.code_len do
-        if utf8.len(self.input[1]) >= i then
+        local actual_input_len = utf8.len(self.input[1])
+        if actual_input_len >= i then
             local char_off = utf8.offset(self.input[1], i)
             local char = self.input[1]:sub(char_off, char_off)
             love.graphics.printf(char, draw_x, draw_y, self.char_w, "center")
         end
-        local line_y = draw_y + self.char_h + 2
-        love.graphics.line(draw_x, line_y, draw_x + self.char_w, line_y)
+
+        if i ~= actual_input_len + 1 or self.caret_flash_timer < 15 then
+            local line_y = draw_y + self.char_h + 2
+            love.graphics.line(draw_x, line_y, draw_x + self.char_w, line_y)
+        end
+
         draw_x = draw_x + self.char_w + self.char_spacing
     end
+
+    self.caret_flash_timer = (self.caret_flash_timer + 1 * DTMULT) % 30
 
     super:draw(self)
 end
