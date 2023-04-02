fd20d7406d3bb84109026ce9f4f4ddee93b6067e
Liu Wenyuan <15816141883@163.com>
2023-04-02 12:26:48 +0800

    Slightly rewrite fountain1

--- scripts/world/maps/fountain1/map.lua
+++ scripts/world/maps/fountain1/map.lua
@@ -1,22 +1,7 @@
 local Fountain1, super = Class(Map)
 
-function Fountain1:load()
-  super:load(self)
-  
-  --local flag = Game:getFlag("hasSealedFirstFountain", default)
-  
-  --if flag == false or flag == default then
-
-	--Game.world:startCutscene("room_prefountain.fountain1")
-	
-
-  --end
-
-end
-
 function Fountain1:onEnter()
-	print("what")
-  	Game.world:startCutscene("room_prefountain.fountain1")
+    Game.world:startCutscene("room_prefountain.fountain1")
 end
 
 return Fountain1
