61c4a685d9e690ca67e24f4119902c3588de269d
Liu Wenyuan <15816141883@163.com>
2023-04-04 22:32:23 +0800

    Hathy uses static random value

--- libraries/Hathy/scripts/battle/cutscenes/hathy.lua
+++ libraries/Hathy/scripts/battle/cutscenes/hathy.lua
@@ -3,10 +3,13 @@ return {
         for _, enemy in ipairs(Game.battle.enemies) do
             enemy:addMercy(100)
         end
+
         cutscene:text("* You ordered Ralsei to flatter all the enemies!")
-        if math.random(1, 3) == 1 then
+
+        local dlg = math.random(1, 3)
+        if dlg == 1 then
             cutscene:text("* Out of anyone,[wait:5] I'm glad we randomly encountered all of you.", "smile", "ralsei")
-        elseif math.random(1, 3) == 2 then
+        elseif dlg == 2 then
             cutscene:text("* I'm making tea later...[wait:10]\n* Umm,[wait:5] would you like any?", "smile", "ralsei")
         else
             cutscene:text("* We don't need EXP.[wait:10]\n* Just encountering your smile is reward enough.", "smile", "ralsei")
@@ -17,11 +20,14 @@ return {
         for _, enemy in ipairs(Game.battle.enemies) do
             enemy:addMercy(100)
         end
+
         cutscene:text("* You ordered Susie to flatter all the enemies!")
+
+        local dlg = math.random(1, 3)
         cutscene:text("* Can't believe my bozo teammates...", "neutral", "susie")
-        if math.random(1, 3) == 1 then
+        if dlg == 1 then
             cutscene:text("* ... came back here just to see YOU guys.", "smile", "susie")
-        elseif math.random(1, 3) == 2 then
+        elseif dlg == 2 then
             cutscene:text("* ... wanted to fight someone like YOU so much.", "smile", "susie")
         else
             cutscene:text("* ... have a THING for weaklings like you.", "smile", "susie")
