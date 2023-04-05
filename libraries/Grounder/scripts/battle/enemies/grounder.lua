a9d0fab63b7e400f5dc67d47e3a1c56e7f82e756
Liu Wenyuan <15816141883@163.com>
2023-04-05 17:59:25 +0800

    holy Fuck

--- libraries/Grounder/scripts/battle/enemies/grounder.lua
+++ libraries/Grounder/scripts/battle/enemies/grounder.lua
@@ -51,7 +51,7 @@ end
 function Grounder:onShortAct(battler, name)
     if name == "Lecture" then
 		self:setAnimation("tired")
-		self.setTired(true)
+		self:setTired(true)
         if battler.chara.id == "kris" then
             return "* You lectured the enemies on eating healthy."
         else
