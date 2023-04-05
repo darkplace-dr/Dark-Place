21145ac8c430fe5100a1f0b4cb18d08867b7ab80
Liu Wenyuan <15816141883@163.com>
2023-04-05 16:51:26 +0800

    Fix mimic death cine

---
+++ scripts/battle/cutscenes/mimic.lua
@@ -0,0 +1,54 @@
+return {
+    dies = function(cutscene, battler, enemy)
+        local susie = cutscene:getCharacter("susie")
+        local dess = cutscene:getCharacter("dess")
+
+        Game.battle:resetAttackers()
+
+        cutscene:wait(1)
+        enemy:shake(5)
+        Assets.playSound("wing")
+
+        if susie then
+            if Mod:isInTheArena() then
+                cutscene:text("* Heh,[wait:5] looks like we've won.", "smile", "susie")
+            else
+                cutscene:text("* Hey uh,[wait:5] something about this feels off...", "sad_frown", "susie")
+            end
+        end
+
+        enemy:shake(5)
+        Assets.playSound("wing")
+        cutscene:wait(1)
+        enemy:shake(5)
+        Assets.playSound("wing")
+        cutscene:wait(1)
+
+        if dess then
+            if Mod:isInTheArena() then
+                cutscene:text("* funny explosion incomming", "kind", "dess")
+            else
+                cutscene:text("* idk looks fine to me lol", "kind", "dess")
+            end
+        end
+
+        enemy:shake(5)
+        Assets.playSound("wing")
+        cutscene:wait(2)
+
+        enemy:explode(0, 0, false)
+        cutscene:wait(2)
+
+        if Mod:isInTheArena() then
+            if susie then
+                cutscene:text("* That was too easy!", "smile", "susie")
+            end
+            if dess then
+                cutscene:text("* gg ez", "condescending", "dess")
+            end
+        else
+            cutscene:text("* ...", "shock", "susie")
+            cutscene:text("* well erm... THAT just happened lol", "condescending", "dess")
+        end
+    end
+}
