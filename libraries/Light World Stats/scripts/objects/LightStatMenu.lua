aedd8b39001f25f45bd99a8fec4f2203db2abd11
Liu Wenyuan <15816141883@163.com>
2023-04-05 15:46:13 +0800

    Refactor LWS

--- libraries/Light World Stats/scripts/objects/LightStatMenu.lua
+++ libraries/Light World Stats/scripts/objects/LightStatMenu.lua
@@ -1,28 +1,14 @@
----@class LightStatMenu : Object
+---@class _LightStatMenu : LightStatMenu
 ---@overload fun(...) : LightStatMenu
-local LightStatMenu, super = Class(Object)
+local LightStatMenu, super = Class(LightStatMenu, true)
 
 function LightStatMenu:init()
-    super.init(self, 212, 76, 298, 370)
-    
-    self.chara = Game.party[1]
+    super.init(self)
+
     self.selected = 1
 
-    self.draw_children_below = 0
-
-    self.font = Assets.getFont("main")
-
-    self.ui_move = Assets.newSound("ui_move")
-    self.ui_select = Assets.newSound("ui_select")
-
-    self.heart_sprite = Assets.getTexture("player/heart_menu")
-    
-    self.arrowLeft = Assets.getTexture("kristal/menu_arrow_left")
-    self.arrowRight = Assets.getTexture("kristal/menu_arrow_right")
-
-    self.bg = UIBox(0, 0, self.width, self.height)
-    self.bg.layer = -1
-    self:addChild(self.bg)
+    self.arrow_left = Assets.getTexture("kristal/menu_arrow_left")
+    self.arrow_right = Assets.getTexture("kristal/menu_arrow_right")
 end
 
 function LightStatMenu:update()
@@ -33,327 +19,58 @@ function LightStatMenu:update()
         return
     end
 
-    if Input.pressed("left") then
-        if self.selected == 1 then
-    
-        
-    
-        elseif self.selected == 2 then
-        
-            self.selected = 1
-            self.ui_move:play()
-            self.chara = Game.party[1]
-    
-        elseif self.selected == 3 then
-    
-            self.selected = 2
-            self.ui_move:play()
-            self.chara = Game.party[2]
-    
-        elseif self.selected == 4 then
-    
-            self.selected = 3
-            self.ui_move:play()
-            self.chara = Game.party[3]
-    
-        elseif self.selected == 5 then
-    
-            self.selected = 4
-            self.ui_move:play()
-            self.chara = Game.party[4]
-    
-    
-    
-    
+    if #Game.party > 1 then
+        if Input.pressed("left") and self.selected > 1 then
+            self.selected = self.selected - 1
         end
-    
-    
-    elseif Input.pressed("right") then
-    
-        if self.selected == 1 then
-    
-            if #Game.party == 1 then
-                
-            else
-                self.selected = 2
-                self.ui_move:play()
-                self.chara = Game.party[2]
-            end
-    
-        elseif self.selected == 2 then
-    
-            if #Game.party == 2 then
-                
-            else
-                self.selected = 3
-                self.ui_move:play()
-                self.chara = Game.party[3]
-            end
-    
-            
-    
-        elseif self.selected == 3 then
-    
-            if #Game.party == 3 then
-                
-            else
-                self.selected = 4
-                self.ui_move:play()
-                self.chara = Game.party[4]
-            end
-    
-        elseif self.selected == 4 then
-    
-            if #Game.party == 4 then
-                
-            else
-                self.selected = 5
-                self.ui_move:play()
-                self.chara = Game.party[5]
-            end
-    
-        elseif self.selected == 5 then
-    
-    
-    
-    
-    
+        if Input.pressed("right") and self.selected < #Game.party then
+            self.selected = self.selected + 1
         end
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     end
 
-
-
-
-
-
-
-
-
-
-    super.update(self)
+    super.super.update(self)
 end
-
-
---[[
-
-
-function LightStatMenu:onKeyPressed(key)
-    self.ui_move:play()
-
-    if key == "left" then
-    if self.selected == 1 then
-
-    
-
-    elseif self.selected == 2 then
-    
-        self.selected = 1
-        self.chara = Game.party[1]
-
-    elseif self.selected == 3 then
-
-        self.selected = 2
-        self.chara = Game.party[2]
-
-    elseif self.selected == 4 then
-
-        self.selected = 3
-        self.chara = Game.party[3]
-
-    elseif self.selected == 5 then
-
-        self.selected = 4
-        self.chara = Game.party[4]
-
-
-
-
-    end
-
-
-elseif key == "right" then
-
-    if self.selected == 1 then
-
-        if #Game.party == 1 then
-            
-        else
-            self.selected = 2
-            self.chara = Game.party[2]
-        end
-
-    elseif self.selected == 2 then
-
-        if #Game.party == 2 then
-            
-        else
-            self.selected = 3
-            self.chara = Game.party[3]
-        end
-
-        
-
-    elseif self.selected == 3 then
-
-        if #Game.party == 3 then
-            
-        else
-            self.selected = 4
-            self.chara = Game.party[4]
-        end
-
-    elseif self.selected == 4 then
-
-        if #Game.party == 4 then
-            
-        else
-            self.selected = 5
-            self.chara = Game.party[5]
-        end
-
-    elseif self.selected == 5 then
-
-
-
-
-
-    end
-
-
-
-
-    end
-
-
-
-
-
-
-
-
-
-
-end
-
-
-]]
 
 function LightStatMenu:draw()
     love.graphics.setFont(self.font)
-
-
-    if self.selected == 1 then
-
-        if #Game.party == 1 then
-            love.graphics.setColor(PALETTE["world_gray"])
-            love.graphics.draw(self.arrowLeft, 4, 20, 0, 1, 1, 0, 0 )
-            love.graphics.setColor(PALETTE["world_gray"])
-            love.graphics.draw(self.arrowRight, 20, 20, 0, 1, 1, 0, 0 )
-        else
-            love.graphics.setColor(PALETTE["world_gray"])
-            love.graphics.draw(self.arrowLeft, 4, 20, 0, 1, 1, 0, 0 )
-            love.graphics.setColor(PALETTE["world_text"])
-            love.graphics.draw(self.arrowRight, 20, 20, 0, 1, 1, 0, 0 )
-        end
-
-    elseif self.selected == 2 then
-
-
-        if #Game.party == 2 then
-            love.graphics.setColor(PALETTE["world_text"])
-            love.graphics.draw(self.arrowLeft, 4, 20, 0, 1, 1, 0, 0 )
-            love.graphics.setColor(PALETTE["world_gray"])
-            love.graphics.draw(self.arrowRight, 20, 20, 0, 1, 1, 0, 0 )
-        else
-            love.graphics.setColor(PALETTE["world_text"])
-            love.graphics.draw(self.arrowLeft, 4, 20, 0, 1, 1, 0, 0 )
-            love.graphics.setColor(PALETTE["world_text"])
-            love.graphics.draw(self.arrowRight, 20, 20, 0, 1, 1, 0, 0 )
-        end
-
-    elseif self.selected == 3 then
-
-        if #Game.party == 3 then
-            love.graphics.setColor(PALETTE["world_text"])
-            love.graphics.draw(self.arrowLeft, 4, 20, 0, 1, 1, 0, 0 )
-            love.graphics.setColor(PALETTE["world_gray"])
-            love.graphics.draw(self.arrowRight, 20, 20, 0, 1, 1, 0, 0 )
-        else
-            love.graphics.setColor(PALETTE["world_text"])
-            love.graphics.draw(self.arrowLeft, 4, 20, 0, 1, 1, 0, 0 )
-            love.graphics.setColor(PALETTE["world_text"])
-            love.graphics.draw(self.arrowRight, 20, 20, 0, 1, 1, 0, 0 )
-        end
-
-    elseif self.selected == 4 then
-
-        if #Game.party == 4 then
-            love.graphics.setColor(PALETTE["world_text"])
-            love.graphics.draw(self.arrowLeft, 4, 20, 0, 1, 1, 0, 0 )
-            love.graphics.setColor(PALETTE["world_gray"])
-            love.graphics.draw(self.arrowRight, 20, 20, 0, 1, 1, 0, 0 )
-        else
-            love.graphics.setColor(PALETTE["world_text"])
-            love.graphics.draw(self.arrowLeft, 4, 20, 0, 1, 1, 0, 0 )
-            love.graphics.setColor(PALETTE["world_text"])
-            love.graphics.draw(self.arrowRight, 20, 20, 0, 1, 1, 0, 0 )
-        end
-
-    elseif self.selected == 5 then
-
-        love.graphics.setColor(PALETTE["world_text"])
-        love.graphics.draw(self.arrowLeft, 4, 20, 0, 1, 1, 0, 0 )
-        love.graphics.setColor(PALETTE["world_gray"])
-        love.graphics.draw(self.arrowRight, 20, 20, 0, 1, 1, 0, 0 )
-
-
-
-
-    end
-
-
-
-
     love.graphics.setColor(PALETTE["world_text"])
 
+    local chara = Game.party[self.selected]
 
-    love.graphics.print("\"" .. self.chara:getName() .. "\"", 36, 8)
+    love.graphics.print("\"" .. chara:getName() .. "\"", 36, 8)
 
-    if self.chara.lwstatsportrait ~= nil then
-        love.graphics.draw(self.chara.lwstatsportrait, 170, 5, 0, 2.5, 2.5, 0, 0 )
+    if chara.lwstatsportrait ~= nil then
+        love.graphics.draw(chara.lwstatsportrait, 170, 5, 0, 2.5, 2.5, 0, 0 )
     end
 
-    love.graphics.print("LV  "..self.chara:getLightLV(), 4, 68)
-    love.graphics.print("HP  "..self.chara:getHealth().." / "..self.chara:getStat("health"), 4, 100)
+    love.graphics.setColor((#Game.party > 1 and self.selected > 1)
+        and PALETTE["world_text"]
+        or PALETTE["world_gray"])
+    love.graphics.draw(self.arrow_left, 4, 20, 0, 1, 1, 0, 0)
+    love.graphics.setColor((#Game.party > 1 and self.selected < #Game.party)
+        and PALETTE["world_text"]
+        or PALETTE["world_gray"])
+    love.graphics.draw(self.arrow_right, 20, 20, 0, 1, 1, 0, 0)
 
-    local exp_needed = math.max(0, self.chara:getLightEXPNeeded(self.chara:getLightLV() + 1) - self.chara:getLightEXP())
+    love.graphics.print("LV  "..chara:getLightLV(), 4, 68)
+    love.graphics.print("HP  "..chara:getHealth().." / "..chara:getStat("health"), 4, 100)
 
-    love.graphics.print("AT  "..self.chara:getBaseStats()["attack"] .." ("..self.chara:getEquipmentBonus("attack") ..")", 4, 164)
-    love.graphics.print("DF  "..self.chara:getBaseStats()["defense"].." ("..self.chara:getEquipmentBonus("defense")..")", 4, 196)
-    love.graphics.print("EXP: "..self.chara:getLightEXP(),   172, 164)
+    local exp_needed = math.max(0, chara:getLightEXPNeeded(chara:getLightLV() + 1) - chara:getLightEXP())
+
+    love.graphics.print("AT  "..chara:getBaseStats()["attack"] .." ("..chara:getEquipmentBonus("attack") ..")", 4, 164)
+    love.graphics.print("DF  "..chara:getBaseStats()["defense"].." ("..chara:getEquipmentBonus("defense")..")", 4, 196)
+    love.graphics.print("EXP: "..chara:getLightEXP(),   172, 164)
     love.graphics.print("NEXT: "..exp_needed, 172, 196)
 
-    local weapon_name = self.chara:getWeapon() and self.chara:getWeapon():getName() or "None"
-    local armor_name = self.chara:getArmor(1) and self.chara:getArmor(1):getName() or "None"
+    local weapon_name = chara:getWeapon() and chara:getWeapon():getName() or "None"
+    local armor_name = chara:getArmor(1) and chara:getArmor(1):getName() or "None"
 
     love.graphics.print("WEAPON: "..weapon_name, 4, 256)
     love.graphics.print("ARMOR: "..armor_name, 4, 288)
 
     love.graphics.print(Game:getConfig("lightCurrency"):upper()..": "..Game.lw_money, 4, 328)
 
-    super.draw(self)
+    super.super.draw(self)
 end
 
 return LightStatMenu
