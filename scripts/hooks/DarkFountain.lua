0367b41c41aeb0efe91384dc1e85ff69cc46489f
Liu Wenyuan <15816141883@163.com>
2023-04-16 13:34:29 +0800

    Make fade less awful

--- scripts/hooks/DarkFountain.lua
+++ scripts/hooks/DarkFountain.lua
@@ -25,15 +25,17 @@ function DarkFountain:update()
         self.siner = self.siner - self.slowdown * DTMULT
         self.bg_siner = self.bg_siner - (self.slowdown / 16) * DTMULT
         self.bg_color = Utils.mergeColor(self.bg_color, COLORS.white, 0.03 * DTMULT)
-    elseif self.adjust == 3 then -- Old version of 2?
+    elseif self.adjust == 3 then
         self.slowdown = Utils.approach(self.slowdown, 1, 0.01 * DTMULT)
         self.siner = self.siner - (self.slowdown * 0.5) * DTMULT
         self.bg_siner = self.bg_siner - (self.slowdown / 24) * DTMULT
         self.hscroll = self.hscroll - (self.slowdown * 0.8) * DTMULT
         self:setColor(Utils.mergeColor(self.bg_color, {fcolor(self.siner / 16, 160 + (math.sin(self.siner / 128) * 60), 255)}, self.slowdown))
         self.bg_color = Utils.mergeColor(self.bg_color, {fcolor(self.siner / 16, 255, (math.sin(self.siner / 64) * 40) + 60)}, self.slowdown * DTMULT)
-    elseif self.adjust == 4 then
+    elseif self.adjust == 4 then -- Custom
         self.bg_color = Utils.mergeColor(self.bg_color, COLORS.white, 0.03 * DTMULT)
+        -- IDK it would look awful if I don't do this
+        self.eyebody = self.eyebody - 0.0125 * DTMULT
     end
 
     self.bg_siner = self.bg_siner + 0.0625 * DTMULT
