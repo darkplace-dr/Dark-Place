14bc5744cc0447bdbc6fc763abbdaf03b2be8b76
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2023-05-05 17:58:13 -0400

    Update mod.lua
    Fixed a small bug with the taunting mechanic where the player would be allowed to taunt during a cutscene.

--- mod.lua
+++ mod.lua
@@ -39,7 +39,7 @@ end
 function Mod:postUpdate()
     local player = Game.party[1]
 
-    if Input.pressed("v", false) and Game.state == "OVERWORLD" and Game.world.menu == nil then
+    if Input.pressed("v", false) and Game.state == "OVERWORLD" and Game.world.menu == nil and not Game.world:hasCutscene() then
         if player:checkArmor("pizza_toque") then
             if self.taunt_timer == 0 then
                 self.taunt_timer = 0.40
