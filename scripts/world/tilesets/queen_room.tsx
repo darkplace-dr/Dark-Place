98c35cb9af02748727b2e2ee937cf339e960e082
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2023-05-13 19:33:32 -0400

    Tileset files for Queen's Room.

---
+++ scripts/world/tilesets/queen_room.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="queen_room" tilewidth="40" tileheight="40" tilecount="143" columns="13">
+ <image source="../../../assets/sprites/tilesets/queen_room.png" width="520" height="440"/>
+</tileset>
