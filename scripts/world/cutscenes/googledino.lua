1c8dfffdf600aa0d8529bf880d8713542fed25ea
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2023-05-15 12:54:22 -0400

    Google Dino cutscene stuff (intro only)
    Will work on outro to fight later.

---
+++ scripts/world/cutscenes/googledino.lua
@@ -0,0 +1,33 @@
+return {
+    begin = function(cutscene, dino)
+        local player = cutscene:getCharacter("YOU")
+        local dino = cutscene:spawnNPC("googledino", 1200, player.y)
+        dino:setSprite("static")
+        dino.sprite.flip_x = true
+		
+
+        cutscene:panTo(1000, y)
+        cutscene:wait(2)
+		
+        cutscene:detachCamera()
+        cutscene:wait(2)
+		
+        dino.sprite.flip_x = false
+		
+        cutscene:wait(1)
+
+        Assets.playSound("dino_jump")
+        Game.world.timer:tween(0.2, dino, {y=player.y-100})
+        cutscene:wait(0.2)
+        Game.world.timer:tween(0.2, dino, {y=player.y})
+        cutscene:wait(0.2)
+        Assets.playSound("dino_jump")
+        Game.world.timer:tween(0.2, dino, {y=player.y-100})
+        cutscene:wait(0.2)
+        Game.world.timer:tween(0.2, dino, {y=player.y})
+
+        cutscene:wait(0.5)
+		
+        cutscene:startEncounter("googledino", nil, {dino})
+    end,
+}
