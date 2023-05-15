378b90576d38d7590c38267447aac378eba749ae
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2023-05-15 14:37:32 -0400

    Whoops, I forgor the tileset files for dinofield.

---
+++ scripts/world/tilesets/dinofield.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="dinofield" tilewidth="40" tileheight="40" tilecount="24" columns="6">
+ <editorsettings>
+  <export target="dinofield.lua" format="lua"/>
+ </editorsettings>
+ <image source="../../../assets/sprites/tilesets/dinofield.png" width="240" height="160"/>
+</tileset>
