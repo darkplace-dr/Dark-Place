6f558cda3d1b673d04370b43fb6e7f80cc796879
DiamondBor <99621601+DiamondBor@users.noreply.github.com>
2023-05-19 21:01:40 -0300

    Added Clouds tileset, updated Floor2.world
    Updated Floor2.world to include all floor 2 maps, added Cloud area tilesets (for something imma do later)

---
+++ scripts/world/tilesets/cloudtiles.lua
@@ -0,0 +1,31 @@
+return {
+  version = "1.10",
+  luaversion = "5.1",
+  tiledversion = "1.10.0",
+  name = "cloudtiles",
+  class = "",
+  tilewidth = 40,
+  tileheight = 40,
+  spacing = 0,
+  margin = 0,
+  columns = 12,
+  image = "../../../assets/sprites/tilesets/bg_cloudtiles_1.png",
+  imagewidth = 480,
+  imageheight = 560,
+  objectalignment = "unspecified",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 40,
+    height = 40
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 168,
+  tiles = {}
+}
---
+++ scripts/world/tilesets/cloudtiles.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="cloudtiles" tilewidth="40" tileheight="40" tilecount="168" columns="12">
+ <editorsettings>
+  <export target="cloudtiles.lua" format="lua"/>
+ </editorsettings>
+ <image source="../../../assets/sprites/tilesets/bg_cloudtiles_1.png" width="480" height="560"/>
+</tileset>
