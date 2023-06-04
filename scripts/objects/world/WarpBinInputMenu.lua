03225f0b8728f81246f3d45c04e32315fba3f287
Liu Wenyuan <15816141883@163.com>
2023-06-04 10:03:33 +0800

    do not maintain our flash timer

--- scripts/objects/world/WarpBinInputMenu.lua
+++ scripts/objects/world/WarpBinInputMenu.lua
@@ -16,9 +16,6 @@ function WarpBinInputMenu:init()
     self.box.debug_select = false
     self:addChild(self.box)
 
-    self.caret_flash_time = 30
-    self.caret_flash_timer = 0
-
     self.font = Assets.getFont("main_mono", 32)
     self.char_w = 32
     self.char_h = self.char_w
@@ -37,13 +34,9 @@ function WarpBinInputMenu:init()
         enter_submits = true,
         text_restriction = function(c)
             if utf8.len(self.input[1]) == self.code_len then return end
-            self.caret_flash_timer = 0
             return c:upper()
         end
     })
-    TextInput.pressed_callback = function(c)
-        self.caret_flash_timer = 0
-    end
     TextInput.text_callback = function()
         self.input[1] = Utils.sub(self.input[1], 1, self.code_len)
     end
@@ -73,16 +66,13 @@ function WarpBinInputMenu:draw()
             love.graphics.printf(char, draw_x, draw_y, self.char_w, "center")
         end
 
-        if i ~= math.min(TextInput.cursor_x + 1, self.code_len)
-            or self.caret_flash_timer <= self.caret_flash_time / 2 then
+        if i ~= math.min(TextInput.cursor_x + 1, self.code_len) or TextInput.flash_timer < 0.5 then
             local line_y = draw_y + self.char_h + 2
             love.graphics.line(draw_x, line_y, draw_x + self.char_w, line_y)
         end
 
         draw_x = draw_x + self.char_w + self.char_spacing
     end
-
-    self.caret_flash_timer = (self.caret_flash_timer + 1 * DTMULT) % self.caret_flash_time
 
     super.draw(self)
 end
