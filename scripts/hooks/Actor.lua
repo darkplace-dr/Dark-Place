28f19ecce1c418b85772b3c50679f52643b4c2e5
Liu Wenyuan <15816141883@163.com>
2023-06-10 22:05:16 +0800

    try 2

--- scripts/hooks/Actor.lua
+++ scripts/hooks/Actor.lua
@@ -2,11 +2,9 @@
 ---@field taunt_sprites string[] A list of sprites to show when this actor taunts
 local Actor, super = Class("Actor", true)
 
-Mod:trace(1)
 function Actor:init()
     super.init(self)
 
-    Mod:trace(1)
     self.taunt_sprites = {}
 end
 
--- scripts/main/ow_taunt.lua
+++ scripts/main/ow_taunt.lua
@@ -14,9 +14,20 @@ function Mod:updateTaunt()
         Game.lock_movement = true
         Assets.playSound("taunt", 0.5, Utils.random(0.9, 1.1))
 
+        --[[local party_member_charas = {}
+        party_member_charas[Game.world.player.actor.id] = Game.world.player
+        for _, follower in ipairs(Game.world.followers) do
+            party_member_charas[follower.actor.id] = follower
+        end]]
+
         for _,chara in ipairs(Game.stage:getObjects(Character)) do
             if not chara.actor then goto continue end
-            local sprites = chara.actor:getTauntSprites()
+            --[[if party_member_charas[chara.actor.id] and party_member_charas[chara.actor.id] ~= chara then
+                goto continue
+            end]]
+
+            -- workaround due of actors being loaded first by registry
+            local sprites = chara.actor.getTauntSprites and chara.actor:getTauntSprites() or chara.actor.taunt_sprites
             if not sprites or #sprites <= 0 then goto continue end
 
             -- the shine effect
@@ -29,7 +40,7 @@ function Mod:updateTaunt()
                 chara:resetSprite()
             end)
             chara:addChild(effect)
-            chara:set(Utils.pick(sprites))
+            chara.sprite:set(Utils.pick(sprites))
 
             ::continue::
         end
