e8c67a317279a64aec39d3eace57a810e4eea003
DeltaVelvet <130409566+DeltaVelvet@users.noreply.github.com>
2023-06-12 23:46:29 -0700

    New Objects + Room

--- scripts/world/tilesets/hoteldoors.tsx
+++ scripts/world/tilesets/hoteldoors.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="hoteldoors" tilewidth="32" tileheight="50" tilecount="2" columns="0">
+<tileset version="1.10" tiledversion="1.10.1" name="hoteldoors" tilewidth="32" tileheight="50" tilecount="3" columns="0">
  <editorsettings>
   <export target="hoteldoors.lua" format="lua"/>
  </editorsettings>
@@ -9,5 +9,8 @@
  </tile>
  <tile id="1">
   <image width="32" height="50" source="../../../assets/sprites/objects/hoteldoors/001.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="50" source="../../../assets/sprites/objects/hoteldoors/002.png"/>
  </tile>
 </tileset>
