d00b79928ed214fee9461d51b89bade1f4d048c8
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-06-20 20:19:43 -0400

    The Everhall

--- scripts/world/tilesets/devroom.tsx
+++ scripts/world/tilesets/devroom.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="devroom" tilewidth="40" tileheight="40" tilecount="36" columns="6">
+<tileset version="1.10" tiledversion="1.10.1" name="devroom" tilewidth="40" tileheight="40" tilecount="72" columns="6">
  <editorsettings>
   <export target="devroom.lua" format="lua"/>
  </editorsettings>
- <image source="../../../assets/sprites/tilesets/devroom.png" width="278" height="240"/>
+ <image source="../../../assets/sprites/tilesets/devroom.png" width="278" height="480"/>
 </tileset>
