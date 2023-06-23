3744ed27f166d79bd8ca7c660ebab0116c994361
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2023-06-22 22:38:22 -0400

    Pizza Tower Secret Area tileset and example map.
    Also fixed the crash in Velvet's hotel room (re-exporting the devroom tileset fixed the issue.)

--- scripts/world/tilesets/devhotelfurniture.tsx
+++ scripts/world/tilesets/devhotelfurniture.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="devhotelfurniture" tilewidth="40" tileheight="40" tilecount="88" columns="22">
+<tileset version="1.9" tiledversion="1.9.0" name="devhotelfurniture" tilewidth="40" tileheight="40" tilecount="88" columns="22">
  <editorsettings>
   <export target="devhotelfurniture.lua" format="lua"/>
  </editorsettings>
---
+++ scripts/world/tilesets/pt_secret.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="pt_secret" tilewidth="40" tileheight="40" tilecount="130" columns="10">
+ <editorsettings>
+  <export target="pt_secret.lua" format="lua"/>
+ </editorsettings>
+ <image source="../../../assets/sprites/tilesets/pt_secret.png" width="400" height="520"/>
+</tileset>
