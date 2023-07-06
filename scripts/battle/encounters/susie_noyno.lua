1193385a3e00ee4b32cb15452814e1d9f0b23b42
Liu Wenyuan <15816141883@163.com>
2023-07-06 20:45:29 +0800

    clup

--- scripts/battle/encounters/susie_noyno.lua
+++ scripts/battle/encounters/susie_noyno.lua
@@ -13,6 +13,23 @@ function SusieNoyno:init()
     self.noy = self:addEnemy("noyno", 540, 270)
 end
 
+function SusieNoyno:onBattleStart()
+    self.sus.sprite.flip_x = true
+    self.noy.sprite.flip_x = true
+    self.sus:setAnimation("battle/intro")
+    self.noy:setAnimation("battle/intro")
+end
+
+function SusieNoyno:onStateChange(old, new)
+    if new == "ACTIONSELECT" then
+        self.sus:setAnimation("battle/idle")
+        self.noy:setAnimation("battle/idle")
+    elseif new == "VICTORY" then
+        self.sus:setAnimation("battle/victory")
+        self.noy:setAnimation("battle/victory")
+    end
+end
+
 function SusieNoyno:onReturnToWorld(events)
     -- check whether the enemies were killed
     if Game.battle.killed then
@@ -24,21 +41,6 @@ function SusieNoyno:onReturnToWorld(events)
                 event:setFlag("dont_load", true)
             end
         end
-    end
-end
-
-function SusieNoyno:onStateChange(old, new)
-    if new == "INTRO" then
-        self.sus:setAnimation("battle/intro")
-        self.sus.sprite.flip_x = true
-        self.noy:setAnimation("battle/intro")
-        self.noy.sprite.flip_x = true
-    elseif new == "ACTIONSELECT" then
-        self.sus:setAnimation("battle/idle")
-        self.noy:setAnimation("battle/idle")
-    elseif new == "VICTORY" then
-        self.sus:setAnimation("battle/victory")
-        self.noy:setAnimation("battle/victory")
     end
 end
 
