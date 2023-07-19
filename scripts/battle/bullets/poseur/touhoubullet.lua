48ba2eb5ab17b9fe2e7a2930ae79b8bcca1ef4c1
Liu Wenyuan <15816141883@163.com>
2023-07-20 06:46:03 +0800

    Yeah

--- scripts/battle/bullets/poseur/touhoubullet.lua
+++ scripts/battle/bullets/poseur/touhoubullet.lua
@@ -19,7 +19,7 @@ function TouhouBullet:update()
     local dtmult60 = DT * 60
 
     local posx = (70 * self.lerp) * math.sin(self.timer * self.negmult + self.offset)
-    local posy = (70 * self.lerp) * math.cos(self.timer + self.offset) - self.y_offset - self.lerp * 50
+    local posy = (70 * self.lerp) * math.cos(self.timer + self.offset) + self.y_offset - self.lerp * 50
     self:setPosition(Game.battle.arena.x + posx, Game.battle.arena.y - posy)
     self.timer = self.timer + 1/40 * dtmult60
     self.lerp = Utils.approach(self.lerp, 4, 1/90 * dtmult60)
--- scripts/battle/waves/poseur/touhou.lua
+++ scripts/battle/waves/poseur/touhou.lua
@@ -7,14 +7,14 @@ function Touhou:init()
 end
 
 function Touhou:onStart()
-	local y_offset = -(180 - 80)
+	local y_offset = 180 - 80
     local mult = 0.5
 
     -- wt += 1 then (if wt%30 == 0) ...
     self.timer:every((30-1)/60, function()
         local numbullets = 10
         for i = 1, numbullets+1 do
-            local bullet = self:spawnBullet("poseur/touhoubullet", Game.battle.arena.x, Game.battle.arena.y + y_offset, y_offset)
+            local bullet = self:spawnBullet("poseur/touhoubullet", Game.battle.arena.x, Game.battle.arena.y - y_offset, y_offset)
             bullet.offset = math.pi * 2 * i / numbullets
             bullet.negmult = mult
         end
