c8e6561f7663a8fd0bf8d5e1f340a128b3a81683
Liu Wenyuan <15816141883@163.com>
2023-07-31 08:40:54 +0800

    More

--- preview/preview.lua
+++ preview/preview.lua
@@ -11,12 +11,12 @@ function preview:init(mod, button, menu)
 
     self.particles = {}
     self.particle_timer = 0
-    self.particle_texture = love.graphics.newImage(self.base_path.."/star.png")
+    self.particle_tex = love.graphics.newImage(self.base_path.."/star.png")
 
     self.swellow = nil
     self.swellow_timer = 0
 
-	self.paul = false
+	self.paul_played = false
     self.paul_stream = nil
 end
 
@@ -38,8 +38,12 @@ function preview:update()
     self.particle_timer = self.particle_timer + DT
     if self.particle_timer >= 0.4 then
         self.particle_timer = 0
-        local radius = Utils.random() * 12
-        table.insert(self.particles, {radius = radius, x = Utils.random() * SCREEN_WIDTH, y = SCREEN_HEIGHT + radius, max_radius = radius, speed = Utils.random() * 0.5 + 0.5})
+        local radius = Utils.random(12)
+        table.insert(self.particles, {
+            radius = radius, max_radius = radius,
+            x = Utils.random(SCREEN_WIDTH), y = SCREEN_HEIGHT + radius,
+            speed = Utils.random(0.5, 1)
+        })
     end
 
     if self:shouldShowSwellow() then
@@ -52,42 +56,43 @@ function preview:update()
     end
 
 	if self:shouldPaul() then
-        if not self.paul then
+        if not self.paul_played then
             if not self.paul_stream then
                 self.paul_stream = love.audio.newSource(self.base_path.."/paul.ogg", "stream")
             end
             self.paul_stream:play()
-            self.paul = true
+            self.paul_played = true
         end
     else
-        if self.paul then
+        if self.paul_played then
             self.paul_stream:stop()
         end
-        self.paul = false
+        self.paul_played = false
     end
 end
 
 function preview:draw()
-    if self.fade > 0 then
-        for _,particle in ipairs(self.particles) do
-            local alpha = (particle.radius / particle.max_radius) * self.fade
+    if self.fade <= 0 then return end
 
-            love.graphics.setColor(1, 1, 1, alpha)
-            love.graphics.draw(self.particle_texture, particle.x, particle.y, particle.radius)
-        end
-        love.graphics.setColor(1, 1, 1)
+    for _,particle in ipairs(self.particles) do
+        local alpha = (particle.radius / particle.max_radius) * self.fade
 
-        if self:shouldShowSwellow() and self.swellow then
-            local alpha = (self.swellow_timer - 1.8) * 0.2
-            ---@type FileNamer
-            local naming_screen = self.menu.naming_screen
-            love.graphics.setColor(1, 1, 1, math.min(alpha, 0.8))
-            love.graphics.draw(self.swellow,
-                SCREEN_WIDTH/2, SCREEN_HEIGHT/2-30, 0,
-                2 + math.max(0, (self.swellow_timer - 3) * 0.02 + naming_screen.whiten), 2,
-                self.swellow:getWidth()/2, self.swellow:getHeight()/2
-            )
-        end
+        love.graphics.setColor(1, 1, 1, alpha)
+        love.graphics.draw(self.particle_tex, particle.x, particle.y, particle.radius)
+    end
+    love.graphics.setColor(1, 1, 1)
+
+    if self:shouldShowSwellow() and self.swellow then
+        local alpha = math.min((self.swellow_timer - 1.8) * 0.2, 0.8)
+        ---@type FileNamer
+        local naming_screen = self.menu.naming_screen
+        local xs_inc = math.max(0, (self.swellow_timer - 3) * 0.02 + naming_screen.whiten)
+        love.graphics.setColor(1, 1, 1, alpha)
+        love.graphics.draw(self.swellow,
+            SCREEN_WIDTH/2, SCREEN_HEIGHT/2-30, 0,
+            2 + xs_inc, 2,
+            self.swellow:getWidth()/2, self.swellow:getHeight()/2
+        )
     end
 end
 
