9f0fa4bc48dee7e6e6cfcb5c6efdfd183cf93066
Liu Wenyuan <15816141883@163.com>
2023-08-01 19:25:15 +0800

    Shine

--- libraries/Shine/scripts/world/events/shine.lua
+++ libraries/Shine/scripts/world/events/shine.lua
@@ -6,7 +6,7 @@ function shine:init(x, y, width, height)
     super.init(self, x, y, width, height)
 
     self:setOrigin(0.5, 0.5)
-    self:setSprite("world/events/spr_shine", 1/4)
+    self:setSprite("world/events/shine", 1/4)
 
     self.solid = true
 end
