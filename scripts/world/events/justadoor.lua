750acacf2f93b1c832f2660a1bc281e2b5ce83e3
SilvazWasTaken <112694330+SilvazWasTaken@users.noreply.github.com>
2023-08-04 18:32:29 +0100

    Add files via upload

---
+++ scripts/world/events/justadoor.lua
@@ -0,0 +1,25 @@
+local justadoor, super = Class(Event)
+
+function justadoor:init(data)
+    super.init(self, data)
+
+    self.sprite = Sprite("world/events/floor2/justadoor")
+    self.sprite:setScale(2)
+    self:addChild(self.sprite)
+
+    self.solid = false
+end
+
+function justadoor:onInteract(player, dir)
+    Game.world:startCutscene(function(cutscene)
+        cutscene:text("* You have a big feeling you shouldn't enter this place.")
+        cutscene:text("* Still enter?")
+		local enter = cutscene:choicer({"Enter", "Do not"})
+		print(enter)
+		if enter == 1 then
+			Game.world:mapTransition("floor2/hell")
+		end
+    end)
+end
+
+return justadoor
