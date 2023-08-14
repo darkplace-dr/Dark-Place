e72f48b2d45b5cfb45e08af918304cf48ad9556e
Liu Wenyuan <15816141883@163.com>
2023-08-14 15:19:41 +0800

    have you considerwed:

--- libraries/Pie/lib.lua
+++ libraries/Pie/lib.lua
@@ -1,8 +1,10 @@
 local Lib = {}
 
 function Lib:init()
-    print("Thank you for using the Pie Library by Mr_Oinky!")
-    print("(Pie v1.2.1 for Kristal v0.8.0)")
+    if Kristal.getLibConfig("pie", "print_console_intro") ~= false then
+        print("Thank you for using the Pie Library by Mr_Oinky!")
+        print("(Pie v1.2.1 for Kristal v0.8.0)")
+    end
 end
 
 return Lib
