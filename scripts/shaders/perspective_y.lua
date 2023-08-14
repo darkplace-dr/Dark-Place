75d5e1da14497d4df355ccfbbebcc85b7928e9e4
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2023-08-14 15:15:35 -0400

    Added perspective_x and perspective_y shaders.

---
+++ scripts/shaders/perspective_y.lua
@@ -0,0 +1,11 @@
+return love.graphics.newShader([[
+// tried and true
+float edge_stretch_str = 1.5; // the higher the more stretched
+float edge_stretch_lim = 1.5; // the higher the less stretched
+
+vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords)
+{
+    texture_coords.y -= (texture_coords.y - .5) * edge_stretch_str * pow(abs(texture_coords.x - .5), edge_stretch_lim);
+    return Texel(tex, texture_coords) * color;
+}
+]])
