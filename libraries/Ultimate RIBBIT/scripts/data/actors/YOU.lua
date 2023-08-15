0ff5de95d21229bbc87a07be68e9600a08f64f0d
Liu Wenyuan <15816141883@163.com>
2023-08-15 20:40:50 +0800

    Revert "move cursed.ogg logic to uribbit"
    This reverts commit 0575ecc5de6ec120c5b3b2cf50a2f6e057b8ced4.

--- libraries/Ultimate RIBBIT/scripts/data/actors/YOU.lua
+++ libraries/Ultimate RIBBIT/scripts/data/actors/YOU.lua
@@ -96,7 +96,7 @@ function actor:init()
         ["fall"] = {-5, -6},
         ["ball"] = {1, 8},
         ["landed"] = {-4, -2},
-
+		
         ["fell"] = {-12, -10},
         ["fell_serious"] = {-12, -5},
         ["disappointed"] = {0, -1},
@@ -105,61 +105,7 @@ function actor:init()
         ["date"] = {-6, 8},
         ["date_flowey"] = {-4, 8},
     }
-
-    self.cursed_song = Music("cursed")
-    self.cursed_song:stop()
 end
 
-function actor:onWorldUpdate()
-    super.onWorldUpdate(self)
-
-    if self.cursed_song:isPlaying() and self.battle_music and not Game.battle then
-        if Game.world.player and Game.world.player.walk_speed == 0 then
-            Game.world.player.walk_speed = self.old_speed
-            self.old_speed = nil
-        end
-        if self.world_music then
-            Game.world.music:resume()
-            self.world_music = nil
-        end
-        self.battle_music = nil
-        self.cursed_song:stop()
-    end
-end
-
-function actor:onSetSprite(sprite, texture, callback)
-    super.onSetSprite(self, sprite, texture, callback)
-    if texture == "cursed" then
-        self.old_speed = Game.world.player.walk_speed
-        Game.world.player.walk_speed = 0
-        if Game.world.music:isPlaying() then
-            self.world_music = true
-            Game.world.music:pause()
-        end
-        if Game.battle and Game.battle.music:isPlaying() then
-            self.battle_music = true
-            Game.battle.music:pause()
-        end
-        self.cursed_song:play()
-    else
-        if Game.world.player and Game.world.player.walk_speed==0 then
-            Game.world.player.walk_speed = self.old_speed
-            self.old_speed = nil
-        end
-        if self.world_music then
-            Game.world.music:resume()
-            self.world_music = nil
-        end
-        if self.battle_music then
-            if Game.battle then
-                Game.battle.music:resume()
-            end
-            self.battle_music = nil
-        end
-        if self.cursed_song:isPlaying() then
-            self.cursed_song:stop()
-        end
-    end
-end
 
 return actor
