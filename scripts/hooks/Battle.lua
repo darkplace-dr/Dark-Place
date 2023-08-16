53113a20c7ec29948e13b866ada9f4bdc013cf33
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2023-08-16 16:01:59 -0400

    EnemyTension library fix.

--- scripts/hooks/Battle.lua
+++ scripts/hooks/Battle.lua
@@ -9,6 +9,64 @@ function Battle:init()
 	self.superpower = false
 end
 
+function Battle:postInit(state, encounter)
+    super.postInit(self, state, encounter)
+
+    -- If the enemy has a TP value defined, create a bar for it.
+    if self.encounter.enemy_tension then
+        self.enemy_tension_bar = EnemyTensionBar(639, 40, true)
+        self:addChild(self.enemy_tension_bar)
+    end
+end
+
+function Battle:showUI()
+    super.showUI(self)
+    if self.enemy_tension_bar then
+        self.enemy_tension_bar:show()
+    end
+end
+
+function Battle:onStateChange(old,new)
+    super.onStateChange(self,old,new)
+
+    if new == "VICTORY" then
+        if self.enemy_tension_bar then
+            self.enemy_tension_bar.animating_in = false
+            self.enemy_tension_bar.shown = false
+            self.enemy_tension_bar.physics.speed_x = 10
+            self.enemy_tension_bar.physics.friction = -0.4
+        end
+    end
+end
+
+---Adds TP to the enemy's TP bar. Doesn't work if no enemy TP bar exists.
+---@param tp integer Amount to give.
+function Battle:giveEnemyTension(tp)
+    self.encounter.enemy_tension = self.encounter.enemy_tension + tp
+end
+
+---Removes TP from the enemy's TP bar. Doesn't work if no enemy TP bar exists.
+---@param tp integer Amount to remove.
+function Battle:removeEnemyTension(tp)
+    if self.encounter.enemy_tension - tp < 0 then
+        self.encounter.enemy_tension = 0
+    else
+        self.encounter.enemy_tension = self.encounter.enemy_tension - tp
+    end
+end
+
+---Returns current enemy TP value. Doesn't work if no enemy TP bar exists.
+---@return integer tp
+function Battle:getEnemyTension()
+    return self.encounter.enemy_tension
+end
+
+---Sets enemy TP to the given value. Doesn't work if no enemy TP bar exists.
+---@param tp integer Amount to set.
+function Battle:setEnemyTension(tp)
+    self.encounter.enemy_tension = tp
+end
+
 function Battle:update()
 	super.update(self)
 	
