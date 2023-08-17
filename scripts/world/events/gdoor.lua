9dd300b0525751dc5dcd6ac7a5426031a2ea65e4
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-08-17 17:28:40 -0400

    Deltagaster rooms start (2/4)

---
+++ scripts/world/events/gdoor.lua
@@ -0,0 +1,22 @@
+---@class GDoor : Event
+---@overload fun(...) : GDoor
+local GDoor, super = Class(Event)
+
+function GDoor:init(data)
+    super:init(self, data.x, data.y, data.w, data.h)
+	
+	properties = data.properties or {}
+	
+	self.map = properties["map"] or "room1"
+	
+	self:setSprite("world/events/gdoor_1")
+	
+	self:setOrigin(0.5,1)
+end
+
+function GDoor:onInteract(player, dir)
+	self:setSprite("world/events/gdoor_2")
+	Game.world:mapTransition(self.map, "entry", "up")
+end
+
+return GDoor
