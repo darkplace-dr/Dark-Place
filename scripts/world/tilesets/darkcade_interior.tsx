c7f42a54311e35d8479855b38856e3b0123d8878
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2023-08-19 15:44:48 -0400

    New VaporLand map and WIP Darkcade interior tileset.

---
+++ scripts/world/tilesets/darkcade_interior.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="darkcade_interior" tilewidth="40" tileheight="40" tilecount="42" columns="7">
+ <editorsettings>
+  <export target="darkcade_interior.lua" format="lua"/>
+ </editorsettings>
+ <image source="../../../assets/sprites/tilesets/darkcade_interior.png" width="280" height="240"/>
+</tileset>
