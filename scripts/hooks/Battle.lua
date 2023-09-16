0fd8679092e60e1c0073ff7699cdfe8809d5598d
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-09-16 19:14:05 -0400

    Better Battles 3.0

--- scripts/hooks/Battle.lua
+++ scripts/hooks/Battle.lua
@@ -69,6 +69,68 @@ function Battle:onStateChange(old,new)
         end
 
         self:showUI()
+	elseif new == "FLEE" then
+        self.current_selecting = 0
+		local flee_complete = false
+
+        if self.tension_bar then
+            self.tension_bar:hide()
+        end
+
+        for _,battler in ipairs(self.party) do
+            battler:setSleeping(false)
+            battler.defending = false
+            battler.action = nil
+
+            if battler.chara:getHealth() <= 0 then
+                battler:revive()
+                battler.chara:setHealth(battler.chara:autoHealAmount())
+            end
+
+			battler:setAnimation("battle/hurt")
+
+            Assets.playSound("defeatrun")
+
+			local sweat = Sprite("effects/defeat/sweat")
+			sweat:setOrigin(0.5, 0.5)
+			sweat:setScale(0.5, 0.5)
+			sweat:play(5/30, true)
+			sweat.layer = 100
+			battler:addChild(sweat)
+
+			Game.battle.timer:after(15/30, function()
+				sweat:remove()
+				battler:getActiveSprite().run_away_2 = true
+				flee_complete = true
+			end)
+
+            local box = self.battle_ui.action_boxes[self:getPartyIndex(battler.chara.id)]
+            box:resetHeadIcon()
+        end
+
+        local flee_text = "* "
+		
+		local flee_list = {
+			"I'm outta here.",
+			"I've got better to do.",
+			"Escaped...",
+			"Don't slow me down."
+		}
+		
+		flee_text = flee_text .. Utils.pick(flee_list)
+
+        win_text = self.encounter:getVictoryText(win_text, self.money, self.xp) or win_text
+		
+        self:battleText(flee_text, function()
+			while not flee_complete do end
+			for _,battler in ipairs(self.party) do
+				battler:getActiveSprite().run_away_2 = false
+				battler.x = battler.x - 160
+			end
+            self:setState("TRANSITIONOUT")
+            self.encounter:onBattleEnd()
+            return true
+        end)
 	end
 end
 
