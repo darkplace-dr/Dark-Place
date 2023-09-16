64717a6bef4bb4593c15ca91345977d1de029e24
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-09-16 19:50:57 -0400

    BB fix

--- libraries/better_battles/scripts/hooks/DamageNumber.lua
+++ libraries/better_battles/scripts/hooks/DamageNumber.lua
@@ -7,22 +7,16 @@ function DamageNumber:init(type, arg, x, y, color)
 
 	if self.type == "tired" then
 		self.font = Assets.getFont("bluenumbers")
-		if self.amount < 0 then
-			self.text = self.amount.."%"
-		else
-			self.text = "+"..self.amount.."%"
-		end
-		self.x = self.x - 41
-	elseif self.type == "mercy" then
-		self.font = Assets.getFont("goldnumbers")
-		if self.amount == 100 then
+		if self.amount >= 100 then
 			self.type = "msg"
 			self.message = "mercy"
 		elseif self.amount < 0 then
 			self.text = self.amount.."%"
+			self.color = {self.color[1] * 0.75, self.color[2] * 0.75, self.color[3] * 0.75}
 		else
 			self.text = "+"..self.amount.."%"
 		end
+		self.x = self.x - 41
 	end
 end
 
--- libraries/better_battles/scripts/hooks/Encounter.lua
+++ libraries/better_battles/scripts/hooks/Encounter.lua
@@ -2,6 +2,15 @@
 ---@overload fun(...) : Encounter
 local Encounter, super = Class("Encounter", true)
 
+function Encounter:init()
+	super:init(self)
+	
+	-- Can the player flee the battle?
+	self.flee = true
+	-- Chance out of 100 that the player can flee this battle (x/100)
+	self.flee_chance = 60
+end
+
 function Encounter:getNextWaves()
     local waves = {}
     for _,enemy in ipairs(Game.battle:getActiveEnemies()) do
