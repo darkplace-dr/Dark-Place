1c3a1d0c14efc9022560eb20f6030839347c4a30
FireRainV <youaredied1234@gmail.com>
2023-09-16 19:08:28 +0300

    Added nametags to the cutscene.

--- scripts/world/cutscenes/missing.lua
+++ scripts/world/cutscenes/missing.lua
@@ -4,17 +4,17 @@ return {
 		local susie = cutscene:getCharacter("susie")
 		local dess = cutscene:getCharacter("dess")
 		if susie then
-			--cutscene:showNametag("Susie")
+			cutscene:showNametag("Susie")
 			cutscene:text("* ...[wait:5] Hey, what's going on here?", "neutral_side", "susie")
 			if dess then
-				--cutscene:showNametag("Dess")
+				cutscene:showNametag("Dess")
 				cutscene:text("* Just some dumb Garry's Mod modder-", "kind", "dess")
 				cutscene:text("* Who didn't think of testing their mod first before releasing it.", "kind", "dess")
 				cutscene:text("* Lame LMAO", "condescending", "dess")
 			end
-			--cutscene:showNametag("Susie")
+			cutscene:showNametag("Susie")
 			cutscene:text("* Let's just hope it won't kill us.", "sus_nervous", "susie")
-			--cutscene:hideNametag()
+			cutscene:hideNametag()
 		end
 	end,
 }
