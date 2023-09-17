defdc2f6a2b1ace3d335c6eeb23001fbef304ab4
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-09-17 10:41:58 -0400

    Jamm dungeon room (4/4)

---
+++ scripts/world/tilesets/jamm_dungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="jamm_dungeon" tilewidth="40" tileheight="40" tilecount="105" columns="7">
+ <image source="../../../assets/sprites/tilesets/jamm_dungeon.png" width="280" height="600"/>
+</tileset>
