eb06fddf4a3190eebe9a750b6513d984e4abcff9
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-09-20 11:08:50 -0400

    Better Battles 5.0 fixes

--- scripts/hooks/Bullet.lua
+++ scripts/hooks/Bullet.lua
@@ -1,10 +1,32 @@
 ---@class Bullet
 local Bullet, super = Class("Bullet", true)
 
+function Bullet:init(x, y, texture)
+	super:init(self, x, y, texture)
+	
+	self.pierce = false
+end
+
 function Bullet:onCollide(soul)
     if not Game.battle.superpower then
 		super.onCollide(self, soul)
 	end
+end
+
+function Bullet:onDamage(soul)
+    local damage = self:getDamage()
+    if damage > 0 then
+		if not self.pierce then
+			local battlers = Game.battle:hurt(damage, false, self:getTarget())
+			soul.inv_timer = self.inv_timer
+			soul:onDamage(self, damage)
+			return battlers
+		end
+		Game.battle:pierce(damage, false, self:getTarget())
+		soul.inv_timer = self.inv_timer
+		soul:onDamage(self, damage)
+    end
+    return {}
 end
 
 return Bullet
