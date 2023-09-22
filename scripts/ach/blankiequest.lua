adc26f0eea8aec82cfdf794e1cc7dcd1c87b8185
DeltaVelvet <130409566+DeltaVelvet@users.noreply.github.com>
2023-09-21 21:33:35 -0700

    Made the Blankie Quest title funniers

--- scripts/ach/blankiequest.lua
+++ scripts/ach/blankiequest.lua
@@ -3,10 +3,10 @@ local Ach, super = Class(Achievement)
 function Ach:init()
     super.init(self)
     
-    self.name = "A Good Night's [[SHUTTING DOWN]]" -- Display name
+    self.name = "Go the [[Fifty-Percent Off]] to sleep" -- Display name
 
     self.icon = "achievements/blankiequest" -- Normal icon
-    self.desc = "Find Spamton's Blankie [TM]." -- Description
+    self.desc = "[Specil Thanks!] for finding my sweet Blankie [tm]!" -- Description
     self.hint = "It seems you can only do this at night..." -- If info hidden is true then this will show up in place of description, used for hints
     self.hidden = true -- Doesn't show up in the menu if not collected
     self.rarity = "Rare" -- An indicator on how difficult this achievement is. "Common", "Uncommon", "Rare", "Epic" "Legendary", "Unique", "Impossible"
