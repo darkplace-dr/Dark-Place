3e770d6106df87648690aa293793f3d35aefbef6
FireRainV <youaredied1234@gmail.com>
2023-09-22 20:26:45 +0300

    In DESSHERE, Fixed Susie and YOU being absent.

--- scripts/world/cutscenes/desslmao.lua
+++ scripts/world/cutscenes/desslmao.lua
@@ -28,22 +28,27 @@ return {
 			}
 		})
 		Assets.stopSound("lowqualityburp")
-		cutscene:text("* Is that the fuckin' uhh 'Kris where the hell are we' guy??", "condescending", "dess")
+        if cutscene:getCharacter("susie") then
+            cutscene:text("* Is that the fuckin' uhh 'Kris where the hell are we' guy??", "condescending", "dess")
 
-		cutscene:showNametag("Susie")
-		cutscene:text("* ...", "annoyed", "susie")
-		cutscene:text("* Well first of all I'm a girl,[wait:10] and second of all I never said that.", "nervous", "susie")
-		cutscene:text("* Wait,[wait:5] how do you even know us??", "nervous_side", "susie")
+            cutscene:showNametag("Susie")
+            cutscene:text("* ...", "annoyed", "susie")
+            cutscene:text("* Well first of all I'm a girl,[wait:10] and second of all I never said that.", "nervous", "susie")
+            cutscene:text("* Wait,[wait:5] how do you even know us??", "nervous_side", "susie")
 
-		cutscene:showNametag("Dess")
-		cutscene:text("* What do you mean I see you say it all the time on like", "condescending", "dess")
-		cutscene:text("* Twitter memes", "condescending", "dess")
+            cutscene:showNametag("Dess")
+            cutscene:text("* What do you mean I see you say it all the time on like", "condescending", "dess")
+            cutscene:text("* Twitter memes", "condescending", "dess")
 
-		cutscene:showNametag("Susie")
-		cutscene:text("* What the hell is Twitter?", "annoyed", "susie")
+            cutscene:showNametag("Susie")
+            cutscene:text("* What the hell is Twitter?", "annoyed", "susie")
 
-		cutscene:showNametag("Dess")
-		cutscene:text("* Man idk anymore", "condescending", "dess")
+            cutscene:showNametag("Dess")
+            cutscene:text("* Man idk anymore", "condescending", "dess")
+        else
+            cutscene:text("* Sup fuckers, DessHere!", "condescending", "dess")
+            cutscene:text("* Now that you're finally here, we can party!", "condescending", "dess")
+        end
 		if cutscene:getCharacter("brandon") then
 			cutscene:text("* Oh hey is that the dude?", "kind", "dess")
 
@@ -94,7 +99,7 @@ return {
 		cutscene:slideTo(leader, leader.x - 700, leader.y - 50, 1)
 		cutscene:wait(1.5)
 
-		if #Game.party >= 4 then
+		if #Game.party >= Game:getFlag("party_max") then
 			local prev_leader_pm = Game.party[1]
 			Game:setFlag("dessRemovedLeader", prev_leader_pm.id)
 			Game:removePartyMember(prev_leader_pm)
@@ -124,7 +129,9 @@ return {
 		Mod:unlockPartyMember("dess")
 
 		local susie_party = Game:getPartyMember("susie")
-		susie_party.opinions.dess = susie_party.opinions.dess - 20
+        if cutscene:getCharacter("susie") then
+            susie_party.opinions.dess = susie_party.opinions.dess - 20
+        end
   end,
 
 	dessgetoverhere = function(cutscene, event)
@@ -133,9 +140,11 @@ return {
 			cutscene:text("* Hey I can do a crazy impression watch this", "condescending", "dess")
 			cutscene:text("* Look at meeee I'm FRISK from UNDERTALE lmao", "calm", "dess")
 
-			cutscene:showNametag("Susie")
-			cutscene:text("* ...", "annoyed", "susie")
-			cutscene:text("* (We need to get rid of her as fast as possible.)", "annoyed", "susie")
+            if cutscene:getCharacter("susie") then
+                cutscene:showNametag("Susie")
+                cutscene:text("* ...", "annoyed", "susie")
+                cutscene:text("* (We need to get rid of her as fast as possible.)", "annoyed", "susie")
+            end
 			if cutscene:getCharacter("brandon") then
 				cutscene:showNametag("Brandon")
 				cutscene:text("* (Agreed.)", "miffed", "brandon")
@@ -145,7 +154,6 @@ return {
 				cutscene:text("* (Are we only agreeing on this now?)", "nervous_left", "jamm")
 			end
 			cutscene:hideNametag()
-
 			Game:setFlag("dessThingy", true)
 			event:remove()
 		else
@@ -171,50 +179,77 @@ return {
 		local dess = cutscene:getCharacter("dess")
 		local brandon = cutscene:getCharacter("brandon")
 		local jamm = cutscene:getCharacter("jamm")
+        local YOU = cutscene:getCharacter("YOU")
 
 		cutscene:detachFollowers()
 		cutscene:detachCamera()
 
 		cutscene:walkTo(leader, dess.x, dess.y+16, 1, "up")
-		cutscene:walkTo(susie, dess.x, dess.y+32, 1, "up")
+        local y = 32
+        if susie then
+            cutscene:walkTo(susie, dess.x, dess.y+y, 1, "up")
+            y = y + 16
+        end
+        if YOU then
+			cutscene:walkTo(YOU, dess.x, dess.y+y, 1, "up")
+            y = y + 16
+		end
 		if brandon then
-			cutscene:walkTo(brandon, dess.x, dess.y+48, 1, "up")
+			cutscene:walkTo(brandon, dess.x, dess.y+y, 1, "up")
+            y = y + 16
 		end
 		if jamm then
-			cutscene:walkTo(jamm, dess.x, dess.y+64, 1, "up")
+			cutscene:walkTo(jamm, dess.x, dess.y+y, 1, "up")
+            y = y + 16
 		end
 
-		cutscene:showNametag("Susie")
-		cutscene:text("* Ugh,[wait:10] alright,[wait:5] is this the last one?!", "angry", "susie")
+        if susie then
+            cutscene:showNametag("Susie")
+            cutscene:text("* Ugh,[wait:10] alright,[wait:5] is this the last one?!", "angry", "susie")
 
-		cutscene:showNametag("Dess")
-		cutscene:text("* Yep", "neutral", "dess")
+            cutscene:showNametag("Dess")
+            cutscene:text("* Yep", "neutral", "dess")
 
-		cutscene:showNametag("Susie")
-		cutscene:text("* Alright...[wait:10] let's finally get outta here.", "annoyed_down", "susie")
+            cutscene:showNametag("Susie")
+            cutscene:text("* Alright...[wait:10] let's finally get outta here.", "annoyed_down", "susie")
+        else
+            cutscene:showNametag("Dess")
+            cutscene:text("* That was fun![wait:5] Finally I have someone to play around with", "condescending", "dess")
+        end
 		cutscene:hideNametag()
 
 		cutscene:wait(1)
 		boss:shake(8, 0)
 		cutscene:wait(2)
 
-		cutscene:showNametag("Susie")
-		cutscene:text("* Did you just see that??", "surprise_frown", "susie")
-		cutscene:text("* Why did it just shake?", "shy_b", "susie")
-
-		cutscene:showNametag("Dess")
-		cutscene:text("* That's normal, all the other ones shake if you hit them", "neutral", "dess")
+        if susie then
+            cutscene:showNametag("Susie")
+            cutscene:text("* Did you just see that??", "surprise_frown", "susie")
+            cutscene:text("* Why did it just shake?", "shy_b", "susie")
+            cutscene:showNametag("Dess")
+            cutscene:text("* That's normal, all the other ones shake if you hit them", "neutral", "dess")
+        else
+            cutscene:showNametag("Dess")
+            cutscene:text("* ...", "kind", "dess")
+        end
 
 		boss:shake(8, 0)
 
-		cutscene:showNametag("Susie")
-		cutscene:text("* But...[wait:10] we didn't hit it!", "shock_down", "susie")
+        if susie then
+            cutscene:showNametag("Susie")
+            cutscene:text("* But...[wait:10] we didn't hit it!", "shock_down", "susie")
+        else
+            cutscene:text("* ...", "neutral", "dess")
+        end
 
 		boss:shake(16, 0)
 
 		cutscene:showNametag("Dess")
-		cutscene:text("* Yo wait you're right!", "wtf_b", "dess")
-
+        if susie then
+            cutscene:text("* Yo wait you're right!", "wtf_b", "dess")
+        else
+            cutscene:text("* Oh shit!", "wtf_b", "dess")
+        end
 		boss:shake(8, 0)
 
 		cutscene:showNametag("???")
@@ -229,7 +264,9 @@ return {
 
 		cutscene:wait(0.8)
 
-		cutscene:look(susie, "down")
+        if susie then
+            cutscene:look(susie, "down")
+        end
 		cutscene:look(dess, "down")
 		cutscene:look(leader, "down")
 		if brandon then
@@ -238,14 +275,19 @@ return {
 		if jamm then
 			cutscene:look(jamm, "down")
 		end
+		if YOU then
+			cutscene:look(YOU, "down")
+		end
 
 		cutscene:wait(1.2)
 		cutscene:showNametag("???")
 		cutscene:text("* I'm sorry![wait:10]\n* I simply couldn't contain myself!")
 		cutscene:text("* Uheehee!")
 
-		cutscene:showNametag("Susie")
-		cutscene:text("* Who the hell are you?!", "angry", "susie")
+        if susie then
+            cutscene:showNametag("Susie")
+            cutscene:text("* Who the hell are you?!", "angry", "susie")
+        end
 
 		cutscene:hideNametag()
 
@@ -255,11 +297,15 @@ return {
 		cutscene:look(boss, "up")
 		boss:fadeTo(1, 0.05)
 		cutscene:wait(1)
-		cutscene:showNametag("???")
-		cutscene:text("* YOU![wait:10] Well,[wait:5] just you.[wait:10] Not YOU.")
+        cutscene:showNametag("???")
+        if susie then
+            cutscene:text("* YOU![wait:10] Well,[wait:5] just you.[wait:10] Not YOU.")
 
-		cutscene:showNametag("Susie")
-		cutscene:text("* Wha-?![wait:10] What the hell??", "surprise_frown", "susie")
+            cutscene:showNametag("Susie")
+            cutscene:text("* Wha-?![wait:10] What the hell??", "surprise_frown", "susie")
+        else
+            cutscene:text("* LOOK at me![wait:10] I'm the Angry Dino Girl!")
+        end
 
 		cutscene:hideNametag()
 
@@ -270,8 +316,12 @@ return {
 		boss:fadeTo(1, 0.05)
 		cutscene:wait(1)
 		cutscene:showNametag("???")
-		cutscene:text("* Oh well,[wait:5] NOW I'm YOU!")
-		cutscene:text("* Or Kris![wait:10] You couldn't tell from here![wait:10] Uheeheehee!")
+        if susie then
+            cutscene:text("* Oh well,[wait:5] NOW I'm YOU!")
+        else
+            cutscene:text("* AND NOW I'm YOU!")
+        end
+		cutscene:text("* Uheeheehee!")
 		cutscene:hideNametag()
 
 		boss:fadeTo(0.2, 0.05)
@@ -286,28 +336,36 @@ return {
 		cutscene:text("* ...Uheehee!")
 		cutscene:text("* You're even worse than me! Uhee!")
 
-		cutscene:showNametag("Susie")
-		cutscene:text("* Uhh,[wait:10] what do they mean by that?", "nervous", "susie")
+        if susie then
+            cutscene:showNametag("Susie")
+            cutscene:text("* Uhh,[wait:10] what do they mean by that?", "nervous", "susie")
 
-		cutscene:showNametag("???")
-		cutscene:text("* Uheehee![wait:10] You're much better!")
+            cutscene:showNametag("???")
+            cutscene:text("* Uheehee![wait:10] You're much better!")
+        end
+        cutscene:showNametag("???")
 		cutscene:text("* It's too easy to be who you want to be!")
 		cutscene:text("* I know that is not the real Dess Holiday!")
 
-		cutscene:showNametag("Susie")
-		cutscene:text("* Huh???", "surprise_frown", "susie")
+        if susie then
+            cutscene:showNametag("Susie")
+            cutscene:text("* Huh???", "surprise_frown", "susie")
 
-		cutscene:showNametag("???")
-		cutscene:text("* Don't act all surprised!")
-		cutscene:text("* I know that you aren't the real Susie either!")
+            cutscene:showNametag("???")
+            cutscene:text("* Don't act all surprised!")
+            cutscene:text("* I know that you aren't the real Susie either!")
 
-		cutscene:showNametag("Susie")
-		cutscene:text("* ...???", "suspicious", "susie")
-		cutscene:text("* Uh.", "suspicious", "susie")
-		cutscene:text("* Alright,[wait:5] let's smash this guy into a pulp.", "teeth_smile", "susie")
+            cutscene:showNametag("Susie")
+            cutscene:text("* ...???", "suspicious", "susie")
+            cutscene:text("* Uh.", "suspicious", "susie")
+            cutscene:text("* Alright,[wait:5] let's smash this guy into a pulp.", "teeth_smile", "susie")
 
-		cutscene:showNametag("Dess")
-		cutscene:text("* Agreed", "neutral", "dess")
+            cutscene:showNametag("Dess")
+            cutscene:text("* Agreed", "neutral", "dess")
+        else
+            cutscene:showNametag("Dess")
+            cutscene:text("* Whatever,[wait:5]I wanna smash you already", "neutral", "dess")
+        end
 
 		if brandon then
 			cutscene:showNametag("Brandon")
@@ -334,49 +392,56 @@ return {
 		cutscene:showNametag("Dess")
 		cutscene:text("* Wow,[wait:10] we really learned a lot about each-other while on this journey.", "genuine", "dess")
 
-		cutscene:showNametag("Susie")
-		cutscene:text("* WH-", "shock", "susie")
-		cutscene:text("* NO!!", "angry", "susie")
-		cutscene:text("* NO!!!!!", "angry_b", "susie")
-		cutscene:text("* You are NOT making this a 'touching' moment!", "angry_c", "susie")
+        if susie then
+            cutscene:showNametag("Susie")
+            cutscene:text("* WH-", "shock", "susie")
+            cutscene:text("* NO!!", "angry", "susie")
+            cutscene:text("* NO!!!!!", "angry_b", "susie")
+            cutscene:text("* You are NOT making this a 'touching' moment!", "angry_c", "susie")
 
-		cutscene:showNametag("Dess")
-		cutscene:text("* No,[wait:5] I really think we really started to value our friendship there.", "genuine", "dess")
-		cutscene:text("* Whaddya say,[wait:5] after this...", "genuine", "dess")
-		cutscene:text("* You and I smoke a ciggie outside of a 7/11?", "kind", "dess")
+            cutscene:showNametag("Dess")
+            cutscene:text("* No,[wait:5] I really think we really started to value our friendship there.", "genuine", "dess")
+            cutscene:text("* Whaddya say,[wait:5] after this...", "genuine", "dess")
+            cutscene:text("* You and I smoke a ciggie outside of a 7/11?", "kind", "dess")
+         
+            cutscene:showNametag("Susie")
+            cutscene:text("* I just want you to know right now...", "suspicious", "susie")
+            cutscene:text("* You are the absolute worst person I've ever met.", "suspicious", "susie")
 
-		cutscene:showNametag("Susie")
-		cutscene:text("* I just want you to know right now...", "suspicious", "susie")
-		cutscene:text("* You are the absolute worst person I've ever met.", "suspicious", "susie")
+            if brandon then
+                cutscene:showNametag("Brandon")
+                cutscene:text("* Now you know how I feel.", "miffed", "brandon")
+            end
 
-		if brandon then
-			cutscene:showNametag("Brandon")
-			cutscene:text("* Now you know how I feel.", "miffed", "brandon")
-		end
+            cutscene:showNametag("Dess")
+            cutscene:text("* Alright but can I stay on the team?", "neutral", "dess")
 
-		cutscene:showNametag("Dess")
-		cutscene:text("* Alright but can I stay on the team?", "neutral", "dess")
+            cutscene:showNametag("Susie")
+            cutscene:text("* NO!", "teeth_b", "susie")
 
-		cutscene:showNametag("Susie")
-		cutscene:text("* NO!", "teeth_b", "susie")
+            cutscene:showNametag("Dess")
+            cutscene:text("* ...", "neutral", "dess")
+            cutscene:text("* Please?", "condescending", "dess")
 
-		cutscene:showNametag("Dess")
-		cutscene:text("* ...", "neutral", "dess")
-		cutscene:text("* Please?", "condescending", "dess")
+            cutscene:showNametag("Susie")
+            cutscene:text("* NO!!![wait:20] NO NO NO!", "teeth_b", "susie")
 
-		cutscene:showNametag("Susie")
-		cutscene:text("* NO!!![wait:20] NO NO NO!", "teeth_b", "susie")
+            cutscene:showNametag("Dess")
+            cutscene:text("* If I do I'll give the other guy's position back as leader", "neutral", "dess")
 
-		cutscene:showNametag("Dess")
-		cutscene:text("* If I do I'll give the other guy's position back as leader", "neutral", "dess")
+            cutscene:showNametag("Susie")
+            cutscene:text("* ...", "teeth_b", "susie")
+            cutscene:text("* ALRIGHT, FINE!", "teeth_b", "susie")
+            cutscene:text("* But I am NOT talking to you anymore!", "angry", "susie")
 
-		cutscene:showNametag("Susie")
-		cutscene:text("* ...", "teeth_b", "susie")
-		cutscene:text("* ALRIGHT, FINE!", "teeth_b", "susie")
-		cutscene:text("* But I am NOT talking to you anymore!", "angry", "susie")
-
-		cutscene:showNametag("Dess")
-		cutscene:text("* Yippeee", "condescending", "dess")
+            cutscene:showNametag("Dess")
+            cutscene:text("* Yippeee", "condescending", "dess")
+        else
+            cutscene:text("* I really think we really started to value our friendship there.", "genuine", "dess")
+            cutscene:text("* Whaddya say,[wait:5] after this...", "genuine", "dess")
+            cutscene:text("* We all smoke a ciggie outside of a 7/11?", "kind", "dess")
+        end
+        
 		if brandon then
 			cutscene:showNametag("Brandon")
 			cutscene:text("* (God damnit.)", "miffed", "brandon")
@@ -388,7 +453,9 @@ return {
 		cutscene:hideNametag()
 
 		local susie_party = Game:getPartyMember("susie")
-		susie_party.opinions.dess = susie_party.opinions.dess - 20
+        if susie then
+            susie_party.opinions.dess = susie_party.opinions.dess - 20
+        end
 
 		cutscene:detachFollowers()
 		Game:movePartyMember("dess", 2)
@@ -402,9 +469,11 @@ return {
 			cutscene:text("* Allowed us to actually kill people normally?", "kind", "dess")
 			cutscene:showNametag("Dess", {top = true})
 			cutscene:text("* That'd be a cool reference to hit Deltarune fangame made by RynoGG know as Deltatraveler where in the section 2 obliteration route you can actually kill the animals and people if you clear out all the enemies in the first few rooms", "condescending", "dess", {top = true})
-			cutscene:showNametag("Susie")
-			cutscene:text("* ...", "neutral_side", "susie")
-			cutscene:text("* Oooookay then...", "neutral", "susie")
+			if susie then
+                cutscene:showNametag("Susie")
+                cutscene:text("* ...", "neutral_side", "susie")
+                cutscene:text("* Oooookay then...", "neutral", "susie")
+            end
 			if brandon then
 				cutscene:showNametag("Brandon")
 				cutscene:text("[speed:0.3]* ...", "frown", "brandon")
