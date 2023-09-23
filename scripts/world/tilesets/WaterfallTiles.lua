4515be77af597f55e6d667194b361d54bf127a95
Agent 7 <ronaldmclamb@gmail.com>
2023-09-22 21:21:09 -0400

    Add party menu to the debug system
    Also reserved the warp bin code "MIRRORED" for a planned area

---
+++ scripts/world/tilesets/WaterfallTiles.lua
@@ -0,0 +1,28 @@
+return {
+  version = "1.5",
+  luaversion = "5.1",
+  tiledversion = "1.8.6",
+  name = "Water Tiles",
+  tilewidth = 40,
+  tileheight = 40,
+  spacing = 0,
+  margin = 0,
+  columns = 14,
+  image = "../../../assets/sprites/tilesets/WaterfallTiles.png",
+  imagewidth = 560,
+  imageheight = 640,
+  objectalignment = "unspecified",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 40,
+    height = 40
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 224,
+  tiles = {}
+}
