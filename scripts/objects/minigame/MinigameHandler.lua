798ae61ec5b88f16e9e2dc7175e5b3bebfa8ac1a
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-10-03 21:39:54 -0400

    Minigame handler (just object, not state yet)

---
+++ scripts/objects/minigame/MinigameHandler.lua
@@ -0,0 +1,19 @@
+local MinigameHandler, super = Class(Object)
+
+function MinigameHandler:init()
+    super.init(self, 0, 0)
+end
+
+function MinigameHandler:update()
+    super.update(self)
+end
+
+function MinigameHandler:draw()
+    super.draw(self)
+end
+
+function MinigameHandler:onKeyPressed(key)
+	
+end
+
+return MinigameHandler
