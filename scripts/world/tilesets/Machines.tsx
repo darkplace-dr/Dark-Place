e43a3717ac826fb334112134a6d610738370ce40
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-10-06 09:14:47 -0400

    Tileset for Darkcade machines (for other places)

---
+++ scripts/world/tilesets/Machines.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Machines" tilewidth="47" tileheight="67" tilecount="1" columns="0">
+ <editorsettings>
+  <export target="Machines.lua" format="lua"/>
+ </editorsettings>
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="47" height="67" source="../../../assets/sprites/minigames/ball_jump.png"/>
+ </tile>
+</tileset>
