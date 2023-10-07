d9437a327d997823bd394b0d1113fcbb19f17f8a
BrandonK7200 <brandonkillman@hotmail.com>
2023-10-07 16:27:55 -0300

    Fixed party opinions not being saved

---
+++ scripts/hooks/PartyMember.lua
@@ -0,0 +1,18 @@
+---@class PartyMember
+local PartyMember, super = Class("PartyMember", true)
+
+function PartyMember:init()
+    super.init(self)
+
+    self.opinions = {}
+end
+
+function PartyMember:onSave(data)
+    data.opinions = self.opinions
+end
+
+function PartyMember:onLoad(data)
+    self.opinions = data.opinions or self.opinions
+end
+
+return PartyMember
