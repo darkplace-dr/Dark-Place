917900d8e7ecf372c07f01b9cd715cd6c272bc7b
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2023-10-07 21:00:48 -0400

    Added a third floor to the elevator.

---
+++ scripts/world/tilesets/floor3-objects.lua
@@ -0,0 +1,77 @@
+return {
+  version = "1.9",
+  luaversion = "5.1",
+  tiledversion = "1.9.0",
+  name = "floor3_objects",
+  class = "",
+  tilewidth = 60,
+  tileheight = 102,
+  spacing = 0,
+  margin = 0,
+  columns = 0,
+  objectalignment = "unspecified",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 1,
+    height = 1
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 8,
+  tiles = {
+    {
+      id = 0,
+      image = "../../../assets/sprites/world/maps/floor3/gr_cacti.png",
+      width = 37,
+      height = 45
+    },
+    {
+      id = 1,
+      image = "../../../assets/sprites/world/maps/floor3/gr_door.png",
+      width = 38,
+      height = 52
+    },
+    {
+      id = 2,
+      image = "../../../assets/sprites/world/maps/floor3/gr_vending.png",
+      width = 30,
+      height = 45
+    },
+    {
+      id = 3,
+      image = "../../../assets/sprites/world/maps/floor3/gr_wires.png",
+      width = 35,
+      height = 20
+    },
+    {
+      id = 4,
+      image = "../../../assets/sprites/world/maps/floor3/gr_table.png",
+      width = 60,
+      height = 36
+    },
+    {
+      id = 5,
+      image = "../../../assets/sprites/world/maps/floor3/gr_sidedoor.png",
+      width = 41,
+      height = 102
+    },
+    {
+      id = 6,
+      image = "../../../assets/sprites/world/maps/floor3/gr_tv.png",
+      width = 58,
+      height = 51
+    },
+    {
+      id = 7,
+      image = "../../../assets/sprites/world/maps/floor3/gr_elevatordoor.png",
+      width = 38,
+      height = 52
+    }
+  }
+}
