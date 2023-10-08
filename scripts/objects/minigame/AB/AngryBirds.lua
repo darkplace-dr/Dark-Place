40483a19c87b09ebd0461832bda02b872bd8d933
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2023-10-07 21:38:36 -0400

    Updated Angry Birds minigame code again.

--- scripts/objects/minigame/AB/AngryBirds.lua
+++ scripts/objects/minigame/AB/AngryBirds.lua
@@ -3,21 +3,23 @@ local AngryBirds, super = Class("MinigameHandler")
 function AngryBirds:init()
     super.init(self)
 	
-    self.state = "LEVELSELECTION", -- "TRANSITION", "LEVELSELECTION", "LEVEL", "PAUSE", "LEVELCLEAR", "LEVELFAIL"
+    self.state = "TRANSITION", -- "TRANSITION", "LEVELSELECTION", "LEVEL", "PAUSE", "LEVELCLEAR", "LEVELFAIL"
 	Assets.playSound("minigames/ab/misc/star_collect")
+
+    self.timer = Timer()
 	
     self.music = Music()
-    self.timer = Timer()
+	
+	self.fade = Rectangle(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, SCREEN_WIDTH, SCREEN_HEIGHT)
+	self.fade:setOrigin(0.5,0.5)
+	self.fade.color = {1,1,1}
+    self.fade.alpha = 0
+	self:addChild(self.fade)
 	
 	self.state_timer = 0
 	
-	self.bg = Rectangle(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 0, 0)
-	self.bg:setOrigin(0.5,0.5)
-	self.bg.color = {1,1,1}
-    self.bg.alpha = 0
-	self:addChild(self.bg)
-	
     self.ls_page = 1
+    self.ls_alpha = 0
 	
     self.ls_bg = Assets.getTexture("minigames/ab/levelselect/bg")
     self.ls_left = Assets.getTexture("minigames/ab/levelselect/left")
@@ -26,16 +28,35 @@ function AngryBirds:init()
     --self.reward_popup = self:spawnObject(ABRewardPopUp())
 end
 
+function AngryBirds:postInit()
+	super.postInit(self)
+end
+
 function AngryBirds:setState(state)
 	self.state = state
 	self.state_timer = 0
 	self:onStateChange(state)
 end
 
-function AngryBirds:onStateChange(state)
-    if state == "TRANSITION" then
-        self.timer:tween()
+function AngryBirds:update()
+    self.state_timer = self.state_timer + DT
+
+    if self.state == "TRANSITION" then
+        self.fade.alpha = self.state_timer
+		if self.state_timer > 2.5 then
+			self:setState("LEVELSELECTION")
+		end
+	elseif self.state == "LEVELSELECTION" then
+		if self.state_timer < 2.5 then
+			self.ls_alpha = self.state_timer
+        else
+            self.ls_alpha = 1
+        end
     end
+    super.update(self)
+end
+
+function AngryBirds:onStateChange(state)
     if state == "LEVELSELECTION" then
         self.music:play("minigames/ab/levelselect")
     end
@@ -44,7 +65,7 @@ end
 function AngryBirds:draw()
     super.draw(self)
 	
-    love.graphics.setColor(1, 1, 1)
+    love.graphics.setColor(1, 1, 1, self.ls_alpha)
     if self.state == "LEVELSELECTION" then
         love.graphics.setColor(143/255, 116/255, 185/255)
         love.graphics.rectangle("fill", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
