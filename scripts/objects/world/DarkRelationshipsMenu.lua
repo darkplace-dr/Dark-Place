e5594d11a7f20aef7900e458450cf74cc5578f98
BrandonK7200 <brandonkillman@hotmail.com>
2023-10-13 00:10:46 -0300

    Relationship menu update (Still WIP)

--- scripts/objects/world/DarkRelationshipsMenu.lua
+++ scripts/objects/world/DarkRelationshipsMenu.lua
@@ -3,9 +3,15 @@ local DarkRelationshipsMenu, super = Class(Object)
 function DarkRelationshipsMenu:init()
     super.init(self, 170, 90, 300, 300)
 
+    self.parallax_x = 0
+    self.parallax_y = 0
+
     self.font = Assets.getFont("main")
+    self.font2 = Assets.getFont("plain")
 
     self.ui_cancel_small = Assets.newSound("ui_cancel_small")
+
+    self.relationship_arrows = Assets.getTexture("ui/menu/relationshiparrows")
 
     self.bg = UIBox(0, 0, self.width, self.height)
     self.bg.layer = -1
@@ -20,6 +26,12 @@ function DarkRelationshipsMenu:draw()
         local head = nil
         if v.id == "YOU" then -- YOU's party sprite path isn't capitalized, so check if it's them specifically
             head = Assets.getTexture("party/you/head_1")
+        elseif v.id == "jamm" then -- Check if on the Dungeonkiller route or not
+            if Game:getFlag("dungeonkiller") and Game:getFlag("jamm_closure") then
+                head = Assets.getTexture("party/jamm/head_shadowed")
+            else
+                head = Assets.getTexture("party/jamm/head")
+            end
         else
             head = Assets.getTexture("party/"..v.id.."/head")
         end
@@ -33,12 +45,25 @@ function DarkRelationshipsMenu:draw()
             love.graphics.draw(head, 225, 250, 0, 2)
         end
     end
-    if #Game.party >= 2 then -- Somebody please make this work thank you
+    if #Game.party >= 2 then
         local member1opinions = Game.party[1].opinions
         local member1 = Game.party[1].id
         local member2opinions = Game.party[2].opinions
         local member2 = Game.party[2].id
-        love.graphics.print(member1opinions[member2] or "This shit wack yo", 150, 30)
+        love.graphics.setFont(self.font2)
+        love.graphics.print(member1opinions[member2], 80, 45)
+        love.graphics.print(member2opinions[member1], 200, 75)
+        love.graphics.draw(self.relationship_arrows, 75, 60, 0, 2)
+        if #Game.party >= 3 then
+            local member3opinions = Game.party[3].opinions
+            local member3 = Game.party[3].id
+            love.graphics.print(member3opinions[member1], 30, 230, math.rad(270))
+            love.graphics.print(member1opinions[member3], 70, 100, math.rad(90))
+            love.graphics.draw(self.relationship_arrows, 60, 90, math.rad(90), 2)
+            love.graphics.print(member3opinions[member2], 80, 220, math.rad(-45))
+            love.graphics.print(member2opinions[member3], 200, 130, math.rad(-45))
+            love.graphics.draw(self.relationship_arrows, 230, 110, math.rad(135), 2.8, 2)
+        end
     end
 end
 
