e5e0709a89676bb780963f26e730a677ec1c3821
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-10-12 23:00:31 -0400

    Post-Dungeonkiller sprites

---
+++ scripts/world/maps/devhotel/rooms/room_003/map.lua
@@ -0,0 +1,10 @@
+local Forest, super = Class(Map)
+
+function Forest:onEnter()
+	super:onEnter(self)
+	if Game:getFlag("dungeonkiller") and Game.world:getEvent(28) then
+		Game.world:getEvent(28):setActor("jamm_hurt")
+	end
+end
+
+return Forest
