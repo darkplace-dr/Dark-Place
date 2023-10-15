74a0f931dddafddec5a6ca56caf40511a70b3d08
Agent 7 <ronaldmclamb@gmail.com>
2023-10-15 18:10:27 -0400

    Fixed Zero persisting after defeating him
    Also added a reward chest at the end of the alley he's in, containing a BP Plus

--- scripts/world/maps/cybercity/alleyzero/map.lua
+++ scripts/world/maps/cybercity/alleyzero/map.lua
@@ -3,6 +3,10 @@ local AlleyZero, super = Class(Map)
 function AlleyZero:onEnter()
     -- Only reason this should happen is if someone debugs into the room.
     --Game.world:loadMap("misc/dogcheck")
+
+    if Game:getFlag("zero_defeated") == true then
+        Game.world.map:getEvent(5):remove()
+    end
     if Game.inventory:isFull("armors") then
         local textbox = Interactable(350, 240, 40, 120, {
             text1 = "* (The 48 ARMORs in your pockets seem to have tripped some kind of security measure.)"
