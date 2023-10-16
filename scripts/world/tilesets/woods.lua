3a59706fa067edcf75ec23969d2ca3907d563ba0
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-10-16 11:46:17 -0400

    Deoxtest

---
+++ scripts/world/tilesets/woods.lua
@@ -0,0 +1,28 @@
+return {
+  version = "1.5",
+  luaversion = "5.1",
+  tiledversion = "1.8.2",
+  name = "woods",
+  tilewidth = 40,
+  tileheight = 40,
+  spacing = 0,
+  margin = 0,
+  columns = 11,
+  image = "../../../assets/sprites/tilesets/woods.png",
+  imagewidth = 440,
+  imageheight = 1640,
+  objectalignment = "unspecified",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 40,
+    height = 40
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 451,
+  tiles = {}
+}
