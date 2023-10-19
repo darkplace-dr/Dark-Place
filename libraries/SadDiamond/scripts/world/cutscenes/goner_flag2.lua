aa97a3501f53c91d45769050d3e216de5cae3d99
SadDiamondMan <139525343+SadDiamondMan@users.noreply.github.com>
2023-10-19 09:59:00 -0400

    removing ALOT of junk

--- libraries/SadDiamond/scripts/world/cutscenes/goner_flag2.lua
+++
@@ -1,7 +0,0 @@
-return {
-    head_1 = function(cutscene, event, party)
-        Game:setFlag("head_1", true)
-        Game:setFlag("head_0", false)
-    end
-}
-
--- libraries/SadDiamond/scripts/world/tilesets/reds_house_2f.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="reds_house_2f" tilewidth="16" tileheight="16" tilecount="110" columns="11" tilerendersize="grid">
- <editorsettings>
-  <export target="reds_house_2f.lua" format="lua"/>
- </editorsettings>
- <image source="../../../assets/sprites/rooms/pokemon/pallet_town/reds_house_2f.png" width="176" height="160"/>
-</tileset>
