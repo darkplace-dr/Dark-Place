76d7ab54a16c1caa1ce25e21669eb6c2442bbc76
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-10-24 18:33:10 -0400

    Booty

---
+++ scripts/battle/waves/booty_stomp.lua
@@ -0,0 +1,41 @@
+local Basic, super = Class(Wave)
+
+function Basic:init()
+	super:init(self)
+	
+	self.arena_width = 90
+	
+	self.time = 10
+end
+
+function Basic:onStart()
+    -- Every 0.33 seconds...
+    self.timer:every(1.5, function()
+		local ran = love.math.random(1,3)
+		
+		for i=1, 3 do
+			-- Our X position is offscreen, to the right
+			local x = Game.battle.arena:getLeft() + 15 + (i-1) * 30
+			-- Get a random Y position between the top and the bottom of the arena
+			local y = -60
+
+			-- Spawn smallbullet going left with speed 8 (see scripts/battle/bullets/smallbullet.lua)
+			local to_spawn = "boot"
+			if i == ran then
+				to_spawn = "boot_cs"
+			end
+			local bullet = self:spawnBullet(to_spawn, x, y)
+
+			-- Dont remove the bullet offscreen, because we spawn it offscreen
+			bullet.remove_offscreen = false
+		end
+    end)
+end
+
+function Basic:update()
+    -- Code here gets called every frame
+
+    super.update(self)
+end
+
+return Basic
---
+++ scripts/battle/waves/booty_surround.lua
@@ -0,0 +1,33 @@
+local Basic, super = Class(Wave)
+
+function Basic:init()
+	super:init(self)
+	
+	self.arena_width = 20
+	self.arena_height = 20
+	
+	self.time = 10
+end
+
+function Basic:onStart()
+    self.timer:every(0.8, function()
+		local ran = love.math.random(0,7)
+		
+		-- x = r(cos(rad)), y = r(sin(rad))
+		local x = Game.battle.soul.x + 500 * math.cos(math.rad(ran * 45))
+		local y = Game.battle.soul.y + 500 * math.sin(math.rad(ran * 45))
+
+		local bullet = self:spawnBullet("boot_cs_2", x, y)
+
+		-- Dont remove the bullet offscreen, because we spawn it offscreen
+		bullet.remove_offscreen = false
+    end)
+end
+
+function Basic:update()
+    -- Code here gets called every frame
+
+    super.update(self)
+end
+
+return Basic
