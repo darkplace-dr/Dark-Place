89e30da775851ad3d1ea5287ea57f4bda8e55a98
Agent 7 <ronaldmclamb@gmail.com>
2023-10-25 20:41:05 -0400

    WE DON'T NEED [Mike]!

---
+++ scripts/world/events/soda.lua
@@ -0,0 +1,17 @@
+local soda, super = Class(Event)
+
+function soda:init(data)
+    super.init(self, data)
+
+    self.sprite = Sprite("world/soda")
+    self.sprite:setScale(2)
+    self.sprite:setOrigin(0, 0.5)
+    self:addChild(self.sprite)
+    self.solid = true
+end
+
+function soda:onInteract(player, dir)
+    Assets.stopAndPlaySound("bigshot")
+end
+
+return soda
