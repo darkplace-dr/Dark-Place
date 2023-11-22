bfd81388c55dd14769ad7eb35fabfe686f5d6ffb
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-11-21 19:28:11 -0500

    Fixed the Waluigi Bug
    A bug where you could be purple without equipping both badges

--- scripts/data/items/badges/w_emblem.lua
+++ scripts/data/items/badges/w_emblem.lua
@@ -49,6 +49,8 @@ function badge:onBadgeRemoved()
 	if (BadgesLib:getBadgeEquipped("l_emblem") > 0) then
 		Game.world.player.emblem = ColorMaskFX({0,1,0}, 0.5)
 		Game.world.player:addFX(Game.world.player.emblem, "emblem")
+	else
+		Game.world.player.emblem = nil
 	end
 end
 
