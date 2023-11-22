dd1b0e0473ad35f4b50c0cfe4d85b0598220595f
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-11-21 19:16:16 -0500

    W Emblem

--- scripts/data/items/badges/w_emblem.lua
+++ scripts/data/items/badges/w_emblem.lua
@@ -1,10 +1,10 @@
-local badge, super = Class("badge", "l_emblem")
+local badge, super = Class("badge", "w_emblem")
 
 function badge:init()
     super.init(self)
 
     -- Display name
-    self.name = "L Emblem"
+    self.name = "W Emblem"
 
     self.type = "badge"
 
@@ -19,30 +19,35 @@ function badge:init()
 end
 
 function badge:update(equipped)
-	if equipped and Game.battle and Game.battle.party and #Game.battle.party > 0 and (not Game.battle.party[1].emblem) and (BadgesLib:getBadgeEquipped("w_emblem") <= 0) then
-		Game.battle.party[1].emblem = ColorMaskFX({0,1,0}, 0.5)
-		Game.battle.party[1]:addFX(Game.battle.party[1].emblem, "emblem")
+	if equipped and Game.battle and #Game.battle.party > 0 and not Game.battle.party[1].emblem then
+		if BadgesLib:getBadgeEquipped("l_emblem") > 0 then
+			Game.battle.party[1].emblem = ColorMaskFX({0.5,0,0.5}, 0.5)
+			Game.battle.party[1]:addFX(Game.battle.party[1].emblem, "emblem")
+		else
+			Game.battle.party[1].emblem = ColorMaskFX({1,1,0}, 0.5)
+			Game.battle.party[1]:addFX(Game.battle.party[1].emblem, "emblem")
+		end
 	end
 end
 
 function badge:onBadgeEquipped()
-	if (BadgesLib:getBadgeEquipped("w_emblem") > 0) then
-		Kristal.Console:log("W Emblem detected")
+	if (BadgesLib:getBadgeEquipped("l_emblem") > 0) then
+		Kristal.Console:log("L Emblem detected")
 	end
     if Game.world.player.emblem then
 		Game.world.player:removeFX("emblem")
 		Game.world.player.emblem = ColorMaskFX({0.5,0,0.5}, 0.5)
 		Game.world.player:addFX(Game.world.player.emblem, "emblem")
 	else
-        Game.world.player.emblem = ColorMaskFX({0,1,0}, 0.5)
+        Game.world.player.emblem = ColorMaskFX({1,1,0}, 0.5)
 		Game.world.player:addFX(Game.world.player.emblem, "emblem")
 	end
 end
 
 function badge:onBadgeRemoved()
 	Game.world.player:removeFX("emblem")
-	if (BadgesLib:getBadgeEquipped("w_emblem") > 0) then
-		Game.world.player.emblem = ColorMaskFX({1,1,0}, 0.5)
+	if (BadgesLib:getBadgeEquipped("l_emblem") > 0) then
+		Game.world.player.emblem = ColorMaskFX({0,1,0}, 0.5)
 		Game.world.player:addFX(Game.world.player.emblem, "emblem")
 	end
 end
