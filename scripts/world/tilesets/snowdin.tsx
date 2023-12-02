1cb539b0369838f2762abcc975bcbedb65d55f88
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-12-02 14:57:23 -0500

    Snowdin tileset

---
+++ scripts/world/tilesets/snowdin.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="snowdin" tilewidth="40" tileheight="40" tilecount="3648" columns="64">
+ <image source="../../../assets/sprites/tilesets/snowdin.png" width="2570" height="2288"/>
+</tileset>
