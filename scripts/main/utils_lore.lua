3ed43a40d663968a500289a059321bea1acccbfb
Liu Wenyuan <15816141883@163.com>
2023-12-02 12:48:54 +0800

    rollFun avoids collision

--- scripts/main/utils_lore.lua
+++ scripts/main/utils_lore.lua
@@ -21,8 +21,21 @@ function Mod:shouldUseVelvetAddisons()
 end
 
 -- Randomizes fun
-function Mod:rollFun()
-    Game:setFlag("fun", love.math.random(1, 100))
+---@param avoid_collide boolean whether to ensure that the new value is different from the last one. defaults to true
+function Mod:rollFun(avoid_collide)
+    if avoid_collide == nil then avoid_collide = true end
+
+    local min, max = 1, 100
+    if not avoid_collide then
+        Game:setFlag("fun", love.math.random(min, max))
+    else
+        local old_fun = Game:getFlag("fun")
+        local new_fun
+        while new_fun == nil or new_fun == old_fun do
+            new_fun = love.math.random(min, max)
+        end
+        Game:setFlag("fun", new_fun)
+    end
 end
 
 -- Adds a party member for selection in the DarkPartyMenu
