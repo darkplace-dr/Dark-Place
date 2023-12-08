6cab580f484710da65652fb0f520290f4d9d3482
Liu Wenyuan <15816141883@163.com>
2023-12-08 22:36:55 +0800

    fix a bit of bugs in spin

--- scripts/main/ow_speen.lua
+++ scripts/main/ow_speen.lua
@@ -32,50 +32,48 @@ function Speen:init()
 end
 
 function Speen:update()
-	-- FIXME: Crashes the mod if used in the first Dess room after Dess bats away your party leader
-	if Input.pressed("s") and not self.is_spinning then
-		if (Game.world and not Game.world.cutscene) and not Game.battle and not Mod.taunt_lock_movement and not OVERLAY_OPEN then
-			self.is_spinning = true
-			self.current_facing = "down"
-			self.timer = FRAMERATE*self.rotate_speed
-			self.lancered = love.math.random(0, 100) <= 25
-			self.beyblade = love.math.random(0, 100) <= 10
-			self.rotat_e = love.math.random(0, 100) <= 5
-			if self.lancered then
-				self.lancer_sound:play()
-				for i=1,4 do
-					local l = Sprite("world/the_boy")
-					l:play(self.rotate_speed)
-					l:setScale(2)
-					l:setLayer(WORLD_LAYERS["top"])
-					Game.world:addChild(l)
-					if i == 1 then
-						l:setScreenPos(0, 0)
-					elseif i == 2 then
-						l:setScreenPos(SCREEN_WIDTH-l.width*2, 0)
-					elseif i == 3 then
-						l:setScreenPos(0, SCREEN_HEIGHT-l.height*2)
-					elseif i == 4 then
-						l:setScreenPos(SCREEN_WIDTH-l.width*2, SCREEN_HEIGHT-l.height*2)
-					end
-					table.insert(self.lancer_sprites, l)
+	if Input.pressed("s", false)
+		and not self.is_spinning
+		and (Game.state == "OVERWORLD" and Game.world.state == "GAMEPLAY" and not Game.world:hasCutscene())
+		and not (Game.lock_movement or self.taunt_lock_movement)
+		and not (OVERLAY_OPEN or TextInput.active)
+	then
+		self.is_spinning = true
+		self.current_facing = "down"
+		self.timer = FRAMERATE*self.rotate_speed
+		self.lancered = love.math.random(0, 100) <= 25
+		self.beyblade = love.math.random(0, 100) <= 10
+		self.rotat_e = love.math.random(0, 100) <= 5
+		if self.lancered then
+			self.lancer_sound:play()
+			for i=1,4 do
+				local l = Sprite("world/the_boy")
+				l:play(self.rotate_speed)
+				l:setScale(2)
+				l:setLayer(WORLD_LAYERS["top"])
+				Game.world:addChild(l)
+				if i == 1 then
+					l:setScreenPos(0, 0)
+				elseif i == 2 then
+					l:setScreenPos(SCREEN_WIDTH-l.width*2, 0)
+				elseif i == 3 then
+					l:setScreenPos(0, SCREEN_HEIGHT-l.height*2)
+				elseif i == 4 then
+					l:setScreenPos(SCREEN_WIDTH-l.width*2, SCREEN_HEIGHT-l.height*2)
 				end
-				Game.world.music:pause()
-			else
-				if self.rotat_e then
-					self.rotat_e_sound:play()
-					Game.world.music:pause()
-				elseif self.beyblade then
-					self.beyblade_sound:play()
-					Game.world.music:pause()
-				else
-					if love.math.random(1, 50) == 1 then
-						self.speen_sound:play()
-					else
-						self.spin_sound:play()
-					end
-				end
+				table.insert(self.lancer_sprites, l)
 			end
+			Game.world.music:pause()
+		elseif self.rotat_e then
+			self.rotat_e_sound:play()
+			Game.world.music:pause()
+		elseif self.beyblade then
+			self.beyblade_sound:play()
+			Game.world.music:pause()
+		elseif love.math.random(1, 50) == 1 then
+			self.speen_sound:play()
+		else
+			self.spin_sound:play()
 		end
 	end
 
@@ -86,10 +84,13 @@ function Speen:update()
 			self.timer = 0
 			for i,member in ipairs(Game.party) do
 				local chara = Game.world:getCharacter(member.id)
+				-- compensate for duplicate actors in the party room
 				if Game.world:getCharacter(member.id, 2) then
-					chara = Game.world:getCharacter(member.id, 2) -- compensate for duplicate actors in the party room
+					chara = Game.world:getCharacter(member.id, 2)
 				end
-				chara.sprite:setFacing(self.current_facing)
+				if chara then
+					chara.sprite:setFacing(self.current_facing)
+				end
 			end
 			self:nextFacing()
 		end
