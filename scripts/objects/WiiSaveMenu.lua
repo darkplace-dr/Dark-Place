ca9e53702c98500414cc31c79a1fea2c124da4b8
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-12-09 11:06:07 -0500

    Wii Save Menu
    This is just in here so when the Wii BIOS mod is released, you can save your progress in the Wii slot. You will not be using it at this point, it's 100% for the Wii BIOS mod.

---
+++ scripts/objects/WiiSaveMenu.lua
@@ -0,0 +1,239 @@
+---@class WiiSaveMenu : Object
+---@overload fun(...) : SaveMenu
+local WiiSaveMenu, super = Class(Object)
+
+function WiiSaveMenu:init(marker)
+    super.init(self, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
+
+    self.parallax_x = 0
+    self.parallax_y = 0
+
+    self.draw_children_below = 0
+
+    self.font = Assets.getFont("main")
+
+    self.ui_select = Assets.newSound("ui_select")
+
+    self.heart_sprite = Assets.getTexture("player/heart")
+    self.divider_sprite = Assets.getTexture("ui/box/dark/top")
+
+    self.main_box = UIBox(124, 130, 391, 154)
+    self.main_box.layer = -1
+    self:addChild(self.main_box)
+
+    self.marker = marker
+
+    -- MAIN, SAVE, SAVED, OVERWRITE
+    self.state = "MAIN"
+
+    self.selected_x = 1
+    self.selected_y = 1
+
+    self.saved_file = nil
+
+    self.saves = {}
+    for i = 1, 3 do
+        self.saves[i] = Kristal.getSaveFile(i)
+    end
+end
+
+function WiiSaveMenu:updateSaveBoxSize()
+    if self.state == "SAVED" then
+        self.save_list.height = 210
+    else
+        self.save_list.height = 258
+    end
+end
+
+function WiiSaveMenu:update()
+    if self.state == "MAIN" then
+        if Input.pressed("cancel") then
+            self:remove()
+            Game.world:closeMenu()
+        end
+        if Input.pressed("left") or Input.pressed("right") then
+            self.selected_x = self.selected_x == 1 and 2 or 1
+        end
+        if Input.pressed("up") or Input.pressed("down") then
+            self.selected_y = self.selected_y == 1 and 2 or 1
+        end
+        if Input.pressed("confirm") then
+            if self.selected_x == 1 and self.selected_y == 1 then
+                self.state = "SAVED"
+
+                Kristal.saveGame(0, Game:save(self.marker))
+                self.saved_file = Kristal.getSaveFile(0)
+
+                Assets.playSound("save")
+            elseif self.selected_x == 2 and self.selected_y == 1 then
+                self:remove()
+                Game.world:closeMenu()
+            elseif self.selected_x == 1 and self.selected_y == 2 then
+                if Game.inventory.storage_enabled then
+                    Input.clear("confirm")
+                    self:remove()
+                    Game.world:closeMenu()
+                    Game.world:openMenu(DarkStorageMenu())
+                end
+            end
+        end
+    elseif self.state == "SAVED" then
+        if Input.pressed("cancel") or Input.pressed("confirm") then
+            self:remove()
+            Game.world:closeMenu()
+        end
+    end
+
+    super.update(self)
+end
+
+function WiiSaveMenu:draw()
+    love.graphics.setFont(self.font)
+	
+    if self.state == "MAIN" then
+        local data = Game:getSavePreview()
+
+        -- Header
+        Draw.setColor(PALETTE["world_text"])
+        love.graphics.print(data.name, 120, 120)
+        love.graphics.print("LV "..data.level, 352, 120)
+
+        local minutes = math.floor(data.playtime / 60)
+        local seconds = math.floor(data.playtime % 60)
+        local time_text = string.format("%d:%02d", minutes, seconds)
+        love.graphics.print(time_text, 520 - self.font:getWidth(time_text), 120)
+
+        -- Room name
+        love.graphics.print(data.room_name, 319.5 - self.font:getWidth(data.room_name)/2, 170)
+
+        -- Buttons
+        love.graphics.print("Save", 170, 220)
+        love.graphics.print("Return", 350, 220)
+        if not Game.inventory.storage_enabled then
+            Draw.setColor(PALETTE["world_gray"])
+        end
+        love.graphics.print("Storage", 170, 260)
+        Draw.setColor(PALETTE["world_gray"])
+        love.graphics.print("Recruits", 350, 260)
+
+        -- Heart
+        local heart_positions_x = {142, 322}
+        local heart_positions_y = {228, 270}
+        Draw.setColor(Game:getSoulColor())
+        Draw.draw(self.heart_sprite, heart_positions_x[self.selected_x], heart_positions_y[self.selected_y])
+    elseif self.state == "SAVED" then
+        local data = Game:getSavePreview()
+
+        -- Header
+        Draw.setColor(PALETTE["world_text_selected"])
+        love.graphics.print(data.name, 120, 120)
+        love.graphics.print("LV "..data.level, 352, 120)
+
+        local minutes = math.floor(data.playtime / 60)
+        local seconds = math.floor(data.playtime % 60)
+        local time_text = string.format("%d:%02d", minutes, seconds)
+        love.graphics.print(time_text, 520 - self.font:getWidth(time_text), 120)
+
+        -- Room name
+        love.graphics.print(data.room_name, 319.5 - self.font:getWidth(data.room_name)/2, 170)
+		
+        love.graphics.print("File saved.", self.main_box.x + 30, self.main_box.y + 90)
+    end
+
+    super.draw(self)
+
+    if self.state == "OVERWRITE" then
+        Draw.setColor(PALETTE["world_text"])
+        local overwrite_text = "Overwrite Slot "..self.selected_y.."?"
+        love.graphics.print(overwrite_text, SCREEN_WIDTH/2 - self.font:getWidth(overwrite_text)/2, 123)
+
+        local function drawOverwriteSave(data, x, y)
+            local w = 478
+
+            -- Header
+            love.graphics.print(data.name, x + (w/2) - self.font:getWidth(data.name)/2, y)
+            love.graphics.print("LV "..data.level, x, y)
+
+            local minutes = math.floor(data.playtime / 60)
+            local seconds = math.floor(data.playtime % 60)
+            local time_text = string.format("%d:%02d", minutes, seconds)
+            love.graphics.print(time_text, x + w - self.font:getWidth(time_text), y)
+
+            -- Room name
+            love.graphics.print(data.room_name, x + (w/2) - self.font:getWidth(data.room_name)/2, y+30)
+        end
+
+        Draw.setColor(PALETTE["world_text"])
+        drawOverwriteSave(self.saves[self.selected_y], 80, 165)
+        Draw.setColor(PALETTE["world_text_selected"])
+        drawOverwriteSave(Game:getSavePreview(), 80, 235)
+
+        if self.selected_x == 1 then
+            Draw.setColor(Game:getSoulColor())
+            Draw.draw(self.heart_sprite, 142, 332)
+
+            Draw.setColor(PALETTE["world_text_selected"])
+        else
+            Draw.setColor(PALETTE["world_text"])
+        end
+        love.graphics.print("Save", 170, 324)
+
+        if self.selected_x == 2 then
+            Draw.setColor(Game:getSoulColor())
+            Draw.draw(self.heart_sprite, 322, 332)
+
+            Draw.setColor(PALETTE["world_text_selected"])
+        else
+            Draw.setColor(PALETTE["world_text"])
+        end
+        love.graphics.print("Return", 350, 324)
+    end
+end
+
+function WiiSaveMenu:drawSaveFile(index, data, x, y, selected, header)
+    if self.saved_file then
+        if self.saved_file == index then
+            Draw.setColor(PALETTE["world_text_selected"])
+        else
+            Draw.setColor(PALETTE["world_save_other"])
+        end
+    else
+        if selected then
+            Draw.setColor(PALETTE["world_text_selected"])
+        else
+            Draw.setColor(PALETTE["world_text"])
+        end
+    end
+    if self.saved_file == index and not header then
+        love.graphics.print("File Saved", x + 180, y + 22)
+    elseif not data then
+        love.graphics.print("New File", x + 193, y + 22)
+        if selected then
+            Draw.setColor(Game:getSoulColor())
+            Draw.draw(self.heart_sprite, x + 161, y + 30)
+        end
+    else
+        if self.saved_file or header then
+            love.graphics.print("LV "..data.level, x + 26, y + 6)
+        else
+            love.graphics.print("LV "..data.level, x + 50, y + 6)
+        end
+
+        love.graphics.print(data.name, x + (493 / 2) - self.font:getWidth(data.name) / 2, y + 6)
+
+        local minutes = math.floor(data.playtime / 60)
+        local seconds = math.floor(data.playtime % 60)
+        local time_text = string.format("%d:%02d", minutes, seconds)
+        love.graphics.print(time_text, x + 467 - self.font:getWidth(time_text), y + 6)
+
+        love.graphics.print(data.room_name, x + (493 / 2) - self.font:getWidth(data.room_name) / 2, y + 38)
+
+        if selected and not header then
+            Draw.setColor(Game:getSoulColor())
+            Draw.draw(self.heart_sprite, x + 18, y + 14)
+        end
+    end
+    Draw.setColor(1, 1, 1)
+end
+
+return WiiSaveMenu
