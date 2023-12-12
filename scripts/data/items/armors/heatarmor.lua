f9307f9b43fd88a7506b80ed6a3c6e3da5715255
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-12-12 15:32:15 -0500

    heatarmor fix

--- scripts/data/items/armors/heatarmor.lua
+++ scripts/data/items/armors/heatarmor.lua
@@ -43,9 +43,9 @@ function item:init()
 
     -- Character reactions
     self.reactions = {
-		jamm = "Toasty..."
+		jamm = "Toasty...",
 		mario = {
-				mario = "But I can just use a fire flower."
+				mario = "But I can just use a fire flower.",
 				jamm = "Eh."
 			}
     }
