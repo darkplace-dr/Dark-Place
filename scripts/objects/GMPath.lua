c883c36934bd2668f76cc6072324c6c30bfb1e9c
Liu Wenyuan <15816141883@163.com>
2023-12-23 17:21:07 +0800

    Rework GMPath into HUMP class

--- scripts/objects/GMPath.lua
+++ scripts/objects/GMPath.lua
@@ -1,150 +1,162 @@
-local self = {}
+---@class GMPath
+local GMPath, super = Class()
+
+---@class GMPath.Point
+---@field x number
+---@field y number
+---@field speed number
 
 --
 
-self.LINEAR = 0
-self.CURVED = 1
+GMPath.LINEAR = 0
+GMPath.CURVED = 1
 
 --
 
-function self:new(options)
+function GMPath:init(options)
+    super.init(self)
+
     options = options or {}
 
-    local path = {
-        type = options["type"] or self.LINEAR,
-        closed = options["closed"] or false,
-        precision = options["precision"] or 4
-    }
-    self:reset(path)
-
-    return path
+    self.type = options["type"] or GMPath.LINEAR
+    self.closed = options["closed"] or false
+    self.precision = options["precision"] or 4
+    self:reset()
 end
 
-function self:reset(path)
-    if not path then return end
-
-    path.usr_points = {}
-    path.int_points = {}
+function GMPath:reset()
+    ---@type GMPath.Point[]
+    self.points = {}
+    ---@type GMPath.Point[]
+    self._points_int = {}
 end
 
 --
 
-function self:createPoint(x, y, speed)
-    return
-    {
+---@param x? number
+---@param y? number
+---@param speed? number
+---@return GMPath.Point
+function GMPath:createPoint(x, y, speed)
+    return {
         x = x or 0,
         y = y or 0,
         speed = speed or 0,
     }
 end
 
-function self:addInternalPoint(path, x, y, speed)
-    if not path then return end
-    table.insert(path.int_points, self:createPoint(x, y, speed))
+---@param x? number
+---@param y? number
+---@param speed? number
+function GMPath:addInternalPoint(x, y, speed)
+    table.insert(self._points_int, self:createPoint(x, y, speed))
 end
 
-function self:addPoint(path, x, y, speed)
-    if not path then return end
-    table.insert(path.usr_points, self:createPoint(x, y, speed))
-    self:processInternal(path)
+---@param x? number
+---@param y? number
+---@param speed? number
+function GMPath:addPoint(x, y, speed)
+    table.insert(self.points, self:createPoint(x, y, speed))
+    self:processInternal()
 end
 
 --
 
-function self:createLinear(path)
-    for i = 1, #path.usr_points do
-        local point = path.usr_points[i]
-        self:addInternalPoint(path, point.x, point.y, point.speed)
+function GMPath:createLinear()
+    for i = 1, #self.points do
+        local point = self.points[i]
+        self:addInternalPoint(point.x, point.y, point.speed)
     end
 
-    if path.closed then
-        local point = path.usr_points[1]
-        self:addInternalPoint(path, point.x, point.y, point.speed)
-    end
-end
-
-local function handlePiece(path, _depth, _x1, _y1, _s1, _x2, _y2, _s2, _x3, _y3, _s3)
-    if _depth == 0 then return end
-
-    local mx = (_x1 + _x2 + _x2 + _x3) / 4.0
-    local my = (_y1 + _y2 + _y2 + _y3) / 4.0
-    local ms = (_s1 + _s2 + _s2 + _s3) / 4.0
-
-    if (_x2 - _x1)^2 + (_y2 - _y1)^2 > 16.0 then
-        handlePiece(path, _depth - 1, _x1, _y1, _s1, (_x2 + _x1) / 2.0, (_y2 + _y1) / 2.0, (_s2 + _s1) / 2.0, mx, my, ms)
-    end
-
-    self:addInternalPoint(path, mx, my, ms)
-
-    if (_x2 - _x3)^2 + (_y2 - _y3)^2 > 16.0 then
-        handlePiece(path, _depth - 1, mx, my, ms, (_x3 + _x2) / 2.0, (_y3 + _y2) / 2.0, (_s3 + _s2) / 2.0, _x3, _y3, _s3)
+    if self.closed then
+        local point = self.points[1]
+        self:addInternalPoint(point.x, point.y, point.speed)
     end
 end
-function self:createCurved(path)
+
+function GMPath:createCurved()
     local n = 0
-    local count = #path.usr_points
+    local count = #self.points
 
     if count == 0 then return end
 
-    if not path.closed then
-        self:addInternalPoint(path, path.usr_points[1].x, path.usr_points[1].y, path.usr_points[1].speed)
+    if not self.closed then
+        self:addInternalPoint(self.points[1].x, self.points[1].y, self.points[1].speed)
     end
 
-    if path.closed then
+    if self.closed then
         n = count - 1
     else
         n = count - 3
     end
 
-    for i = 0, n do
-        local point1 = path.usr_points[(i % count) + 1]
-        local point2 = path.usr_points[((i + 1) % count) + 1]
-        local point3 = path.usr_points[((i + 2) % count) + 1]
+    local function handlePiece(depth, x1, y1, s1, x2, y2, s2, x3, y3, s3)
+        if depth == 0 then return end
 
-        handlePiece(path,
-            path.precision,
+        local mx = (x1 + x2 + x2 + x3) / 4.0
+        local my = (y1 + y2 + y2 + y3) / 4.0
+        local ms = (s1 + s2 + s2 + s3) / 4.0
+
+        if (x2 - x1)^2 + (y2 - y1)^2 > 16.0 then
+            handlePiece(depth - 1, x1, y1, s1, (x2 + x1) / 2.0, (y2 + y1) / 2.0, (s2 + s1) / 2.0, mx, my, ms)
+        end
+
+        self:addInternalPoint(mx, my, ms)
+
+        if (x2 - x3)^2 + (y2 - y3)^2 > 16.0 then
+            handlePiece(depth - 1, mx, my, ms, (x3 + x2) / 2.0, (y3 + y2) / 2.0, (s3 + s2) / 2.0, x3, y3, s3)
+        end
+    end
+
+    for i = 0, n do
+        local point1 = self.points[(i % count) + 1]
+        local point2 = self.points[((i + 1) % count) + 1]
+        local point3 = self.points[((i + 2) % count) + 1]
+
+        handlePiece(
+            self.precision,
             (point1.x + point2.x) / 2.0, (point1.y + point2.y) / 2.0, (point1.speed + point2.speed) / 2.0,
             point2.x, point2.y, point2.speed,
             (point2.x + point3.x) / 2.0, (point2.y + point3.y) / 2.0, (point2.speed + point3.speed) / 2.0
         )
     end
 
-    if not path.closed then
-        self:addInternalPoint(path, path.usr_points[count].x, path.usr_points[count].y, path.usr_points[count].speed)
+    if not self.closed then
+        self:addInternalPoint(self.points[count].x, self.points[count].y, self.points[count].speed)
     else
-        self:addInternalPoint(path, path.int_points[1].x, path.int_points[1].y, path.int_points[1].speed)
+        self:addInternalPoint(self._points_int[1].x, self._points_int[1].y, self._points_int[1].speed)
     end
 end
 
-function self:processInternal(path)
-    if not path or #path.usr_points == 0 then return end
-    path.int_points = {}
+function GMPath:processInternal()
+    if #self.points == 0 then return end
+    self._points_int = {}
 
-    if path.type == self.CURVED then
-        self:createCurved(path)
+    if self.type == GMPath.CURVED then
+        self:createCurved()
     else
-        self:createLinear(path)
+        self:createLinear()
     end
 end
 
 --
 
-function self:getPosition(path, pos)
-    if not path or #path.int_points < 1 then return end
+function GMPath:getPosition(pos)
+    if #self._points_int < 1 then return end
     pos = pos or 0
 
-    if pos <= 0 or #path.int_points < 2 then
-        return path.int_points[1]
+    if pos <= 0 or #self._points_int < 2 then
+        return self._points_int[1]
     end
     if pos >= 1 then
-        return path.int_points[#path.int_points]
+        return self._points_int[#self._points_int]
     end
 
-    local idx = math.floor(pos * (#path.int_points - 1))
-    pos = (pos * (#path.int_points - 1)) % 1
+    local idx = math.floor(pos * (#self._points_int - 1))
+    pos = (pos * (#self._points_int - 1)) % 1
 
-    local point_a = path.int_points[idx + 1]
-    local point_b = path.int_points[idx + 2]
+    local point_a = self._points_int[idx + 1]
+    local point_b = self._points_int[idx + 2]
 
     return self:createPoint(
         point_a.x + pos * (point_b.x - point_a.x),
@@ -155,4 +167,4 @@ end
 
 --
 
-return self
+return GMPath
