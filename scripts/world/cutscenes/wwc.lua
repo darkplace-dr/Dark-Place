2baaf5bf2036abcb2cfdffa77bec8e31c8bdc9de
BrandonK7200 <brandonkillman@hotmail.com>
2023-12-26 15:04:24 -0400

    Updated some WWC related stuff

---
+++ scripts/world/cutscenes/wwc.lua
@@ -0,0 +1,56 @@
+return {
+    sign = function(cutscene, event)
+        cutscene:text("* There is a sign here,[wait:5] it reads:")
+        cutscene:text("* Travelers,[wait:5] welcome to what remains of my world.")
+        cutscene:text("* The complete destruction of reality is something only I and a few others have escaped.")
+        cutscene:text("* I hope that whatever has befell on my reality doesn't befell yours.")
+        cutscene:text("* Take what remains of my essence,[wait:5] and do your strongest.")
+        if cutscene:getCharacter("susie") then
+            cutscene:showNametag("susie")
+            cutscene:text("* The complete destruction of reality?", "nervous_side", "susie")
+            cutscene:text("* The hell does this person mean by that?", "nervous", "susie")
+        end
+        if cutscene:getCharacter("brandon") then
+            cutscene:showNametag("Brandon")
+            cutscene:text("* ...", "shaded_b", "brandon")
+        end
+        cutscene:hideNametag()
+    end,
+
+    placeholder = function(cutscene, event)
+        cutscene:text("* A powerful essence shines here.")
+        cutscene:text("* What boon shall you recieve from it?")
+        local option = cutscene:choicer({"Offense", "Defense"})
+        if option == 1 then
+            local success = Game.inventory:tryGiveItem("aeonlance")
+            if success then
+                Game:setFlag("galacta_defeated", true)
+                Assets.stopAndPlaySound("item")
+                cutscene:text("* You recieved the AeonLance.")
+                if not Game:getFlag("shadow_crystals") then
+                    Game:setFlag("shadow_crystals", 1)
+                else
+                    Game:addFlag("shadow_crystals", 1)
+                end
+                cutscene:text("* You got a ShadowCrystal.")
+            else
+                cutscene:text("* Your inventory is too full.")
+            end
+        else
+            local success = Game.inventory:tryGiveItem("originshield")
+            if success then
+                Game:setFlag("galacta_defeated", true)
+                Assets.stopAndPlaySound("item")
+                cutscene:text("* You recieved the OriginShield.")
+                if not Game:getFlag("shadow_crystals") then
+                    Game:setFlag("shadow_crystals", 1)
+                else
+                    Game:addFlag("shadow_crystals", 1)
+                end
+                cutscene:text("* You got a ShadowCrystal.")
+            else
+                cutscene:text("* Your inventory is too full.")
+            end
+        end
+    end
+}
--- scripts/world/maps/dinder.lua
+++ scripts/world/maps/dinder.lua
@@ -9,11 +9,11 @@ return {
   height = 20,
   tilewidth = 40,
   tileheight = 40,
-  nextlayerid = 6,
-  nextobjectid = 26,
+  nextlayerid = 7,
+  nextobjectid = 30,
   properties = {
-    ["border"] = "glow",
-    ["music"] = "deltarune/dogcheck"
+    ["border"] = "simple",
+    ["music"] = "deltarune/wind_highplace"
   },
   tilesets = {
     {
@@ -24,28 +24,18 @@ return {
     },
     {
       name = "city_alleyway",
-      firstgid = 47,
+      firstgid = 54,
       filename = "../tilesets/city_alleyway.tsx",
       exportfilename = "../tilesets/city_alleyway.lua"
+    },
+    {
+      name = "devroom",
+      firstgid = 369,
+      filename = "../tilesets/devroom.tsx",
+      exportfilename = "../tilesets/devroom.lua"
     }
   },
   layers = {
-    {
-      type = "imagelayer",
-      image = "../../../assets/sprites/world/maps/dinder.jpg",
-      id = 2,
-      name = "dinder",
-      class = "",
-      visible = true,
-      opacity = 1,
-      offsetx = 25,
-      offsety = 22.5,
-      parallaxx = 1,
-      parallaxy = 1,
-      repeatx = false,
-      repeaty = false,
-      properties = {}
-    },
     {
       type = "tilelayer",
       x = 0,
@@ -64,26 +54,26 @@ return {
       properties = {},
       encoding = "lua",
       data = {
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 268, 269, 270, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 283, 284, 285, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376,
+        376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376,
+        376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376,
+        376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376,
+        376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376,
+        376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376,
+        376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376,
+        376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376,
+        376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376,
+        376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376,
+        376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376,
+        376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376,
+        376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376,
+        376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376,
+        376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376,
+        376, 376, 376, 376, 376, 376, 376, 376, 376, 275, 276, 277, 376, 376, 376, 376, 376, 376, 376, 376,
+        376, 376, 376, 376, 376, 376, 376, 376, 376, 290, 291, 292, 376, 376, 376, 376, 376, 376, 376, 376,
+        376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376,
+        376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376,
+        376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376, 376
       }
     },
     {
@@ -194,188 +184,8 @@ return {
           rotation = 0,
           visible = true,
           properties = {
-            ["solid"] = true,
-            ["text1"] = "* Heya,[wait:5] welcome to the Dinder room![wait:10] Aka,[wait:5] the half-baked WWC 1 year celebration room!",
-            ["text10"] = "* Even during my toughest times.",
-            ["text11"] = "* I know WWC is nowhere close to being finished...",
-            ["text12"] = "* But I still would like to thank all of you.",
-            ["text13"] = "* - BrandonK7200",
-            ["text14"] = "* P.S:[wait:10] If you get a Maxim Tomato instead of a weapon/armor...",
-            ["text15"] = "* It's because the item hasn't been made yet.",
-            ["text16"] = "* Rest assured,[wait:5] they'll be added eventually.",
-            ["text2"] = "* Here,[wait:5] you'll find several items that are planned for When Worlds Collide.",
-            ["text3"] = "* Specifically,[wait:5] the weapons and armors given out by each section's secret boss.",
-            ["text4"] = "* Along side a chest containing a butt ton of money,[wait:5] as a way of saying thanks.",
-            ["text5"] = "* Thanks to everyone who is,[wait:5] has,[wait:5] and will help out with WWC.",
-            ["text6"] = "* Without you guys,[wait:5] WWC could never get as far as I wanted it to.",
-            ["text7"] = "* Thanks to the entire Kristal engine dev team.",
-            ["text8"] = "* Without your hard work,[wait:5] WWC would most likely never have seen the light of day.",
-            ["text9"] = "* Thanks to all of my friends who've helped me through a lot."
-          }
-        },
-        {
-          id = 7,
-          name = "chest",
-          class = "",
-          shape = "rectangle",
-          x = 320,
-          y = 480,
-          width = 40,
-          height = 40,
-          rotation = 0,
-          visible = true,
-          properties = {
-            ["item"] = "maximtomato"
-          }
-        },
-        {
-          id = 8,
-          name = "chest",
-          class = "",
-          shape = "rectangle",
-          x = 480,
-          y = 480,
-          width = 40,
-          height = 40,
-          rotation = 0,
-          visible = true,
-          properties = {
-            ["item"] = "maximtomato"
-          }
-        },
-        {
-          id = 9,
-          name = "chest",
-          class = "",
-          shape = "rectangle",
-          x = 400,
-          y = 240,
-          width = 40,
-          height = 40,
-          rotation = 0,
-          visible = true,
-          properties = {
-            ["item"] = "originshield"
-          }
-        },
-        {
-          id = 10,
-          name = "chest",
-          class = "",
-          shape = "rectangle",
-          x = 280,
-          y = 360,
-          width = 40,
-          height = 40,
-          rotation = 0,
-          visible = true,
-          properties = {
-            ["item"] = "maximtomato"
-          }
-        },
-        {
-          id = 11,
-          name = "chest",
-          class = "",
-          shape = "rectangle",
-          x = 520,
-          y = 360,
-          width = 40,
-          height = 40,
-          rotation = 0,
-          visible = true,
-          properties = {
-            ["item"] = "maximtomato"
-          }
-        },
-        {
-          id = 17,
-          name = "chest",
-          class = "",
-          shape = "rectangle",
-          x = 400,
-          y = 160,
-          width = 40,
-          height = 40,
-          rotation = 0,
-          visible = true,
-          properties = {
-            ["item"] = "aeonlance"
-          }
-        },
-        {
-          id = 19,
-          name = "chest",
-          class = "",
-          shape = "rectangle",
-          x = 600,
-          y = 360,
-          width = 40,
-          height = 40,
-          rotation = 0,
-          visible = true,
-          properties = {
-            ["item"] = "maximtomato"
-          }
-        },
-        {
-          id = 20,
-          name = "chest",
-          class = "",
-          shape = "rectangle",
-          x = 560,
-          y = 560,
-          width = 40,
-          height = 40,
-          rotation = 0,
-          visible = true,
-          properties = {
-            ["item"] = "maximtomato"
-          }
-        },
-        {
-          id = 21,
-          name = "chest",
-          class = "",
-          shape = "rectangle",
-          x = 240,
-          y = 560,
-          width = 40,
-          height = 40,
-          rotation = 0,
-          visible = true,
-          properties = {
-            ["item"] = "maximtomato"
-          }
-        },
-        {
-          id = 22,
-          name = "chest",
-          class = "",
-          shape = "rectangle",
-          x = 200,
-          y = 360,
-          width = 40,
-          height = 40,
-          rotation = 0,
-          visible = true,
-          properties = {
-            ["item"] = "maximtomato"
-          }
-        },
-        {
-          id = 23,
-          name = "chest",
-          class = "",
-          shape = "rectangle",
-          x = 400,
-          y = 440,
-          width = 40,
-          height = 40,
-          rotation = 0,
-          visible = true,
-          properties = {
-            ["money"] = "9999"
+            ["cutscene"] = "wwc.sign",
+            ["solid"] = true
           }
         },
         {
@@ -393,6 +203,35 @@ return {
             ["cutscene"] = "spamroom.warpbin",
             ["solid"] = true
           }
+        },
+        {
+          id = 26,
+          name = "interactable",
+          class = "",
+          shape = "rectangle",
+          x = 400,
+          y = 200,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["cutscene"] = "wwc.placeholder",
+            ["solid"] = true
+          }
+        },
+        {
+          id = 28,
+          name = "shine",
+          class = "",
+          shape = "point",
+          x = 420,
+          y = 220,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = true,
+          properties = {}
         }
       }
     },
@@ -422,6 +261,38 @@ return {
           rotation = 0,
           visible = true,
           properties = {}
+        }
+      }
+    },
+    {
+      type = "objectgroup",
+      draworder = "topdown",
+      id = 6,
+      name = "controllers",
+      class = "",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      objects = {
+        {
+          id = 29,
+          name = "toggle",
+          class = "",
+          shape = "point",
+          x = 480,
+          y = 160,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["flag"] = "!galacta_defeated",
+            ["target"] = { id = 28 }
+          }
         }
       }
     }
