1e7fb271a164d96dd6666722ca6c28d78ea32d25
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2023-12-27 20:48:18 -0500

    PartyMember light skills

--- scripts/hooks/PartyMember.lua
+++ scripts/hooks/PartyMember.lua
@@ -193,6 +193,128 @@ function PartyMember:getSkills()
     }
 end
 
+function PartyMember:hasLightSkills()
+    return (self:hasAct() and self:hasSpells())
+end
+
+function PartyMember:getLightSkills()
+    local color = {1, 1, 1, 1}
+    for _,spell in ipairs(self:getSpells()) do
+        if spell:hasTag("spare_tired") and spell:isUsable(spell) and spell:getTPCost(spell) <= Game:getTension() then
+            local has_tired = false
+            for _,enemy in ipairs(Game.battle:getActiveEnemies()) do
+                if enemy.tired then
+                    has_tired = true
+                    break
+                end
+            end
+            if has_tired then
+                color = {0, 178/255, 1, 1}
+            end
+        end
+    end
+    return {
+        {"ACT", "Do all\nsorts of\nthings", nil, function() Game.battle:setState("ENEMYSELECT", "ACT") end},
+        {"Spell", Kristal.getLibConfig("better_battles", "magic_description") or "Cast\nSpells", color, function()
+            Game.battle:clearMenuItems()
+			Game.battle.current_menu_columns = 2
+			Game.battle.current_menu_rows = 3
+
+			if Game.battle.encounter.default_xactions and Game.battle.party[1].chara:hasXAct() then
+				local spell = {
+					["name"] = Game.battle.enemies[1]:getXAction(Game.battle.party[1]),
+					["target"] = "xact",
+					["id"] = 0,
+					["default"] = true,
+					["party"] = {},
+					["tp"] = 0
+				}
+
+				Game.battle:addMenuItem({
+					["name"] = Game.battle.party[1].chara:getXActName() or "X-Action",
+					["tp"] = 0,
+					["color"] = { Game.battle.party[1].chara:getXActColor() },
+					["data"] = spell,
+					["callback"] = function(menu_item)
+						Game.battle.selected_xaction = spell
+						Game.battle:setState("XACTENEMYSELECT", "SPELL")
+					end
+				})
+			end
+
+			for id, action in ipairs(Game.battle.xactions) do
+				if action.party == Game.battle.party[1].chara.id then
+					local spell = {
+						["name"] = action.name,
+						["target"] = "xact",
+						["id"] = id,
+						["default"] = false,
+						["party"] = {},
+						["tp"] = action.tp or 0
+					}
+
+					Game.battle:addMenuItem({
+						["name"] = action.name,
+						["tp"] = action.tp or 0,
+						["description"] = action.description,
+						["color"] = action.color or { 1, 1, 1, 1 },
+						["data"] = spell,
+						["callback"] = function(menu_item)
+							Game.battle.selected_xaction = spell
+							Game.battle:setState("XACTENEMYSELECT", "SPELL")
+						end
+					})
+				end
+			end
+
+			-- Now, register SPELLs as menu items.
+			for _, spell in ipairs(Game.battle.party[1].chara:getSpells()) do
+				local color = spell.color or { 1, 1, 1, 1 }
+				if spell:hasTag("spare_tired") then
+					local has_tired = false
+					for _, enemy in ipairs(Game.battle:getActiveEnemies()) do
+						if enemy.tired then
+							has_tired = true
+							break
+						end
+					end
+					if has_tired then
+						color = { 0, 178 / 255, 1, 1 }
+					end
+				end
+				Game.battle:addMenuItem({
+					["name"] = spell:getName(),
+					["tp"] = spell:getTPCost(Game.battle.party[1].chara),
+					["unusable"] = not spell:isUsable(Game.battle.party[1].chara),
+					["description"] = spell:getBattleDescription(),
+					["party"] = spell.party,
+					["color"] = color,
+					["data"] = spell,
+					["callback"] = function(menu_item)
+						Game.battle.selected_spell = menu_item
+
+						if not spell.target or spell.target == "none" then
+							Game.battle:pushAction("SPELL", nil, menu_item)
+						elseif spell.target == "ally" and #Game.battle.party == 1 then
+							Game.battle:pushAction("SPELL", Game.battle.party[1], menu_item)
+						elseif spell.target == "ally" then
+							Game.battle:setState("PARTYSELECT", "SPELL")
+						elseif spell.target == "enemy" then
+							Game.battle:setState("ENEMYSELECT", "SPELL")
+						elseif spell.target == "party" then
+							Game.battle:pushAction("SPELL", Game.battle.party, menu_item)
+						elseif spell.target == "enemies" then
+							Game.battle:pushAction("SPELL", Game.battle:getActiveEnemies(), menu_item)
+						end
+					end
+				})
+			end
+
+			Game.battle:setState("MENUSELECT", "SPELL")
+        end}
+    }
+end
+
 -- Getters
 
 function PartyMember:getLevel()
