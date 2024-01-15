92cc2c0f1c6b1fab1bd4580096d510684b1f99c9
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2024-01-15 11:14:54 -0500

    Deoxynn prize

---
+++ scripts/data/items/armors/victory_bell.lua
@@ -0,0 +1,49 @@
+local item, super = Class(Item, "victory_bell")
+
+function item:init()
+    super:init(self)
+
+    -- Display name
+    self.name = "VictoryBell"
+
+    -- Item type (item, key, weapon, armor)
+    self.type = "armor"
+    -- Item icon (for equipment)
+    self.icon = "ui/menu/icon/armor"
+
+    -- Battle description
+    self.effect = ""
+    -- Shop description
+    self.shop = ""
+    -- Menu description
+    self.description = "Heals you 5 HP after battle (for the most part)."
+
+    -- Shop buy price
+    self.buy_price = 0
+    -- Shop sell price (set to nil)
+    self.sell_price = nil
+
+    -- Consumable target mode (party, enemy, noselect, or none/nil)
+    self.target = nil
+    -- Where this item can be used (world, battle, all, or none/nil)
+    self.usable_in = "all"
+    -- Item this item will get turned into when consumed
+    self.result_item = nil
+    -- Will this item be instantly consumed in battles?
+    self.instant = false
+
+    -- Equip bonuses (for weapons and armor)
+    self.bonuses = {}
+	
+    -- Bonus name and icon (displayed in equip menu)
+    self.bonus_name = "VictoryBell"
+    self.bonus_icon = "plus"
+
+    -- Equippable characters (default true for armors, false for weapons)
+    self.can_equip = {}
+
+    -- Character reactions
+    self.reactions = {}
+end
+
+return item
