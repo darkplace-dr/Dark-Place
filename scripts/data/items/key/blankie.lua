3ea5b937652d18fe11a0da557cea7200a21ca3c7
Liu Wenyuan <15816141883@163.com>
2024-04-04 11:43:00 +0800

    items/key/blankie: I ate it

--- scripts/data/items/key/blankie.lua
+++ scripts/data/items/key/blankie.lua
@@ -7,7 +7,7 @@ function item:init()
     self.type = "item"
     self.description = "A light blue fleece blanket with a sun and cloud pattern. What was this for again?"
 
-    self.usable_in = "none"
+    self.usable_in = "world"
 
     self.price = 50
 end
