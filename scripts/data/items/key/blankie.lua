a4616c6c10a18c1d568da835aa7fe725dde5d2fe
Liu Wenyuan <15816141883@163.com>
2024-04-04 11:37:55 +0800

    items/key/blankie: Degrade it to a normal item

--- scripts/data/items/key/blankie.lua
+++ scripts/data/items/key/blankie.lua
@@ -4,7 +4,7 @@ function item:init()
     super.init(self)
 
     self.name = "Fleece Blanket"
-    self.type = "key"
+    self.type = "item"
     self.description = "A light blue fleece blanket with a sun and cloud pattern. What was this for again?"
 
     self.usable_in = "none"
