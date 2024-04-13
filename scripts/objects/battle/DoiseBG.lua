6174c4843132edf332a441093ab08ef1d22e1d12
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2024-04-13 12:22:20 -0400

    Updated the Doise background.

--- scripts/objects/battle/DoiseBG.lua
+++ scripts/objects/battle/DoiseBG.lua
@@ -6,7 +6,7 @@ function BG:init()
     local bg1 = Sprite("battle/doise_battlebg1")
 	bg1.wrap_texture_x = true
 	bg1.wrap_texture_y = true
-	bg1:setScale(1)
+	bg1:setScale(2)
 	bg1.physics.speed_x = 0.5
 	bg1.physics.speed_y = 0.5
 	self:addChild(bg1)
@@ -14,7 +14,7 @@ function BG:init()
     local bg2 = Sprite("battle/doise_battlebg2")
 	bg2.wrap_texture_x = true
 	bg2.wrap_texture_y = true
-	bg2:setScale(1)
+	bg2:setScale(2)
 	bg2.physics.speed_x = -1
 	bg2.physics.speed_y = -1
 	self:addChild(bg2)
