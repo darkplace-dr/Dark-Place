8b2b842a7cf234f65d7a116a541c433c936d0498
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2024-04-14 15:25:53 -0400

    Updated AngryBirdsButton.lua

--- scripts/objects/minigame/AB/AngryBirdsButton.lua
+++ scripts/objects/minigame/AB/AngryBirdsButton.lua
@@ -1,21 +1,26 @@
 local AngryBirdsButton, super = Class(Object)
 
-function AngryBirdsButton:init(x, y, image, sound)
+function AngryBirdsButton:init(x, y, sx, sy, s_begin, s_end, image, sound)
 	super:init(self, x, y, 0, 0)
 	
+    self.scale_x, self.scale_y = sx, sy
+    self.scale_begin = s_begin
+    self.scale_end = s_end
+	
 	if image then
-		self.path = "minigames/ab/ui/buttons/" .. image
+		self.image_path = "minigames/ab/ui/buttons/" .. image
 		
-		self.sprite = Sprite(self.path)
+		self.sprite = Sprite(self.image_path)
 		self:addChild(self.sprite)
 		self:setOrigin(0.5,0.5)
 		
 		self.width = self.sprite.width
 		self.height = self.sprite.height
 	end
-
-    self.sound = "minigames/ab/" .. sound
-	self.played_sound = false
+    if sound then
+        self.sound_path = "minigames/ab/" .. sound
+		self.sound = self.sound_path
+	end
 end
 
 function AngryBirdsButton:update() 
@@ -24,30 +29,26 @@ function AngryBirdsButton:update()
 	local mx, my = love.mouse.getPosition()
 	local screen_x, screen_y = self:getScreenPos()
 	screen_x, screen_y = screen_x-self.width/2, screen_y-self.height/2
-	if not self.pressed then
-		if (mx / Kristal.getGameScale() > screen_x) and (mx / Kristal.getGameScale() < (screen_x + self.width)) and (my / Kristal.getGameScale() > screen_y) and (my / Kristal.getGameScale() < (screen_y + self.height)) and self:canHover() then
-			if self:canClick() then
-				if self.scale_x < 1.15 then
-					self.scale_x = self.scale_x + 0.1*DTMULT
-					self.scale_y = self.scale_y + 0.1*DTMULT
-				end
-				if not self.played_sound then
-					self.played_sound = true
-				end
-				if not self.pressed and love.mouse.isDown(1) then
-					self.pressed = true
-					self:onClick()
-				end
+	--if not self.pressed then
+	if (mx / Kristal.getGameScale() > screen_x) and (mx / Kristal.getGameScale() < (screen_x + self.width)) and (my / Kristal.getGameScale() > screen_y) and (my / Kristal.getGameScale() < (screen_y + self.height)) and self:canHover() then
+		if self:canClick() then
+            if self.scale_x < self.scale_end then
+                self.scale_x = self.scale_x + DT
+                self.scale_y = self.scale_y + DT
+            end
+			if not self.pressed and love.mouse.isDown(1) then
+				self.pressed = true
+				self:onClick()
 			end
-		else
-			if self.scale_x > 1 then
-				self.scale_x = self.scale_x - 0.1*DTMULT
-				self.scale_y = self.scale_y - 0.1*DTMULT
-			end
-			self.played_sound = false
 		end
 	else
+		if self.scale_x > self.scale_begin then
+			self.scale_x = self.scale_x - DT
+			self.scale_y = self.scale_y - DT
+		end
 	end
+	--else
+	--end
 end
 
 function AngryBirdsButton:draw() super:draw(self) end
