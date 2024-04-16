1c9d7a3a78a9251089dd1c6c02b24624c8324b56
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2024-04-16 17:23:35 -0400

    Removed old Hometown library.

--- libraries/hometown/scripts/world/tilesets/bg_schooltiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="bg_schooltiles" tilewidth="20" tileheight="20" tilecount="90" columns="9" tilerendersize="grid">
- <image source="../../../assets/sprites/tilesets/hometown/school/bg_schooltiles.png" width="180" height="200"/>
-</tileset>
