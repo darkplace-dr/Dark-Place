9bb7e643f110bd20e20c9075a8f2d7cb86da34bb
Liu Wenyuan <15816141883@163.com>
2024-05-01 17:49:31 +0800

    JukeboxMenu
    - Refactor
    - Remove usage line entirely, giving more space for the list
    - Add a shit ton of album covers

--- libraries/JukeboxMenu/scripts/objects/world/JukeboxMenu.lua
+++ libraries/JukeboxMenu/scripts/objects/world/JukeboxMenu.lua
@@ -40,33 +40,32 @@ function JukeboxMenu:init()
     end
 
     self.none_text = "---"
-	self.none_album = "default"
+    self.none_album = "default"
     self.default_song = {
         name = nil,
         file = nil,
         composer = nil,
         released = nil,
         origin = nil,
-		locked = nil,
-		album = nil
+        locked = nil,
+        album = nil
     }
 
-    self.selected_index = 1
-    self.selected_index_memory = {}
-    self.page = 1
-    self.max_pages = math.ceil(#self.songs / 6)
-    self.songs_per_page = 6
-end
-
-function JukeboxMenu:getPage(page)
-    local start_index = 1 + (page-1) * self.songs_per_page
-    return {unpack(self.songs, start_index, math.min(start_index + self.songs_per_page, #self.songs))}
+    self.pages = {}
+    self.page_index = 1
+    self.songs_per_page = 7
+    self.selected_index = {}
+    for page = 1, math.ceil(#self.songs / self.songs_per_page) do
+        local start_index = 1 + (page-1) * self.songs_per_page
+        self.pages[page] = {unpack(self.songs, start_index, math.min(start_index + self.songs_per_page, #self.songs))}
+        self.selected_index[page] = 1
+    end
 end
 
 function JukeboxMenu:draw()
     super.draw(self)
 
-	love.graphics.setColor(1, 1, 1)
+    love.graphics.setColor(1, 1, 1)
     love.graphics.setLineWidth(4)
 
     love.graphics.setFont(self.font)
@@ -81,44 +80,47 @@ function JukeboxMenu:draw()
     love.graphics.setLineWidth(4)
     love.graphics.setColor(1, 1, 1)
 
-    local cur_page = self:getPage(self.page)
+    local page = self.pages[self.page_index]
     for i = 1, self.songs_per_page do
-        local song = cur_page[i] or self.default_song
+        local song = page[i] or self.default_song
         local name = song.name or self.none_text
-		if song.locked then name = "Locked" end
-		love.graphics.setColor(1, 1, 1)
-		if not song.file or song.locked then
-		    love.graphics.setColor(0.5, 0.5, 0.5)
-		end
-        local scale_x = math.min(196 / self.font:getWidth(name), 1)
+        if song.locked then name = "Locked" end
+        love.graphics.setColor(1, 1, 1)
+        if not song.file or song.locked then
+            love.graphics.setColor(0.5, 0.5, 0.5)
+        end
+        local scale_x = math.min(math.floor(196 / self.font:getWidth(name)), 1)
         love.graphics.print(name, 40, 43 + 40 * (i - 1), 0, scale_x, 1)
     end
 
     love.graphics.setColor(0.4, 0.4, 0.4)
     love.graphics.setFont(self.font_2)
-    love.graphics.printf("Page "..self.page.."/"..self.max_pages, -16, 295, 276, "center")
+    love.graphics.printf("Page "..self.page_index.."/"..#self.pages, -16, (43 + 40 * (self.songs_per_page - 1)) + 60, 276, "center")
     love.graphics.setColor(1, 1, 1)
     love.graphics.setFont(self.font)
 
+    --[[
     love.graphics.rectangle("line", -16, 330, 276, 1)
 
     love.graphics.print("[X] Back", 0, 340)
     love.graphics.printf("[C] Info", 0, 340, 250, "right")
+    ]]
 
     love.graphics.rectangle("line", 260, 20, 1, 356)
 
-    local cur_song = cur_page[self.selected_index] or self.default_song
+    local song = page[self.selected_index[self.page_index]] or self.default_song
 
     love.graphics.setColor(1, 1, 1)
-    local album_art = cur_song.album or self.none_album
-    if not cur_song.file or cur_song.locked then
-        album_art = self.none_album
+    local album_art_path = song.album or self.none_album
+    if not song.file or song.locked then
+        album_art_path = self.none_album
     end
-    love.graphics.draw(Assets.getTexture("albums/"..album_art), 410, 170, 0, 1, 1, 125, 125)
+    local album_art = Assets.getTexture("albums/"..album_art_path)
+    love.graphics.draw(album_art, 410, 170, 0, 1, 1, album_art:getWidth()/2, album_art:getHeight()/2)
 
     local info_font = self.font
     local info_scale = 0.5
-    if cur_song.cjk_info then
+    if song.cjk_info then
         info_font = self.font_cjk
         info_scale = 1
     end
@@ -126,9 +128,9 @@ function JukeboxMenu:draw()
     local info_w = 260 / info_scale
     local info = string.format(
         "Composer: %s\nReleased: %s\nOrigin: %s",
-        cur_song.composer or self.none_text,
-        cur_song.released or self.none_text,
-        cur_song.origin or self.none_text
+        song.composer or self.none_text,
+        song.released or self.none_text,
+        song.origin or self.none_text
     )
     local _, info_lines = info_font:getWrap(info, info_w)
     local info_yoff = info_font:getHeight() * #info_lines * info_scale
@@ -145,7 +147,7 @@ function JukeboxMenu:update()
     if not OVERLAY_OPEN then
         --play song
         if Input.pressed("confirm", false) then
-            local song = self:getPage(self.page)[self.selected_index] or self.default_song
+            local song = self.pages[self.page_index][self.selected_index[self.page_index]] or self.default_song
 
             if not song._locked_explicit then
                 song.locked = not Mod:evaluateCond(song, self)
@@ -164,48 +166,40 @@ function JukeboxMenu:update()
             Game.world:closeMenu()
         end
 
-        local page_bak = self.page
         --page left
         if Input.pressed("left", true) then
             Assets.playSound("ui_move")
-            self.page = self.page - 1
+            self.page_index = self.page_index - 1
         end
         --page right
         if Input.pressed("right", true) then
             Assets.playSound("ui_move")
-            self.page = self.page + 1
+            self.page_index = self.page_index + 1
         end
-        self.page = Utils.clampWrap(self.page, 1, self.max_pages)
-        if self.page ~= page_bak then
-            self.selected_index = self.selected_index_memory[self.page] or 1
-        end
+        self.page_index = Utils.clampWrap(self.page_index, 1, #self.pages)
 
-        local page_ndx = self:getPage(self.page)
+        local page = self.pages[self.page_index]
         --move up
         if Input.pressed("up", true) then
             Assets.playSound("ui_move")
-            if page_ndx[warpIndex(self.selected_index - 1)] then
-                self.selected_index = self.selected_index - 1
-            else
-                self.selected_index = #page_ndx
+            self.selected_index[self.page_index] = warpIndex(self.selected_index[self.page_index] - 1)
+            while not page[self.selected_index[self.page_index]] do
+                self.selected_index[self.page_index] = warpIndex(self.selected_index[self.page_index] - 1)
             end
         end
         --move down
         if Input.pressed("down", true) then
             Assets.playSound("ui_move")
-            if page_ndx[warpIndex(self.selected_index + 1)] then
-                self.selected_index = self.selected_index + 1
-            else
-                self.selected_index = 1
+            self.selected_index[self.page_index] = warpIndex(self.selected_index[self.page_index] + 1)
+            while not page[self.selected_index[self.page_index]] do
+                self.selected_index[self.page_index] = warpIndex(self.selected_index[self.page_index] + 1)
             end
         end
+        --self.selected_index[self.page_index] = warpIndex(self.selected_index[self.page_index])
     end
 
-    self.selected_index = warpIndex(self.selected_index)
-    self.selected_index_memory[self.page] = self.selected_index
-
     --soul positions
-    self.heart_target_y = 60 + 40 * (self.selected_index - 1)
+    self.heart_target_y = 60 + 40 * (self.selected_index[self.page_index] - 1)
     if math.abs(self.heart_target_x - self.heart.x) <= 2 then
         self.heart.x = self.heart_target_x
     end
