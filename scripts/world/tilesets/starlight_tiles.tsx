888f9c64cfd192b1267f84992f1c70f3eb607f20
frenchfry <shorttoast21@gmail.com>
2024-05-09 14:35:31 +1000

    starfart isles begining

---
+++ scripts/world/tilesets/starlight_tiles.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="starlight_tiles" tilewidth="40" tileheight="40" tilecount="900" columns="25">
+ <editorsettings>
+  <export target="starlight_tiles.lua" format="lua"/>
+ </editorsettings>
+ <image source="../../../assets/sprites/tilesets/starlight.png" width="1000" height="1440"/>
+</tileset>
