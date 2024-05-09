04129910902b621da3d790b38029419fc2df8ae6
frenchfry <shorttoast21@gmail.com>
2024-05-10 08:17:54 +1000

    finished room 1

---
+++ scripts/world/tilesets/dark_ruins3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="dark_ruins3" tilewidth="40" tileheight="40" tilecount="75" columns="5">
+ <image source="../../../assets/sprites/tilesets/dark_ruins3.png" width="200" height="600"/>
+</tileset>
---
+++ scripts/world/tilesets/dark_ruins_stuff.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="dark_ruins_stuff" tilewidth="40" tileheight="40" tilecount="16" columns="4">
+ <image source="../../../assets/sprites/tilesets/dark_ruins_stuff.png" width="160" height="160"/>
+</tileset>
