717965fcd7f14e2c1178bddee9d9fbec83e3adb0
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2024-05-19 16:11:09 -0400

    Mario Mario Mario

--- scripts/data/actors/mario_dark_transition.lua
+++ scripts/data/actors/mario_dark_transition.lua
@@ -1,17 +1,17 @@
-local actor, super = Class(Actor, "jamm_dark_transition")
+local actor, super = Class(Actor, "mario_dark_transition")
 
 function actor:init()
     super.init(self)
 
     -- Display name (optional)
-    self.name = "Jamm"
+    self.name = "Mario"
 
     -- Width and height for this actor, used to determine its center
     self.width = 19
     self.height = 37
 
     -- Path to this actor's sprites (defaults to "")
-    self.path = "party/jamm/dark_transition"
+    self.path = "party/mario/dark_transition"
     -- This actor's default sprite or animation, relative to the path (defaults to "")
     self.default = "run"
 end
