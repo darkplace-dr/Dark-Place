40623bcae614143fb3b85d381701f623c9e5144a
Liu Wenyuan <15816141883@163.com>
2024-06-22 10:28:00 +0800

    utils_general, pc: Swap hasSaveFiles 2nd and 3rd param, that didn't make sense

--- scripts/world/cutscenes/pc.lua
+++ scripts/world/cutscenes/pc.lua
@@ -39,7 +39,7 @@ return function(cutscene, event, chara)
 									table.insert(new_gifts, game)
 								end
 							else
-								if Mod:hasSaveFiles(game:sub(start+1), data.fused_identify) then
+								if Mod:hasSaveFiles(game:sub(start+1), nil, data.fused_identify) then
 									table.insert(new_gifts, game)
 								end
 							end
