2ce304bfcb3063a6a943b0879f9d28f358c66b93
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2024-07-01 20:47:45 -0400

    Super Star

---
+++ scripts/world/events/superstar.lua
@@ -0,0 +1,22 @@
+local SuperStar, super = Class(Event)
+
+function SuperStar:init(data)
+    super.init(self, data.x, data.y, 16, 16, data.properties)
+	self:setOrigin(0.5,0.5)
+	self:setSprite("world/events/star")
+    self.solid = false
+end
+
+function SuperStar:onCollide(chara)
+    if chara.is_player then
+		if chara.inv_timer <= 0 then
+			chara.old_song = Game.world.music.current
+			Game.world.music:play("starman/" .. Game.party[1]:getStarmanTheme())
+		end
+        chara.inv_timer = 15
+		chara.invincible_colors = true
+		self:remove()
+    end
+end
+
+return SuperStar
