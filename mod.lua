7304b5415b9ecc30be83f271e61a77de839b5aed
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2024-07-01 20:57:08 -0400

    Super Star

--- mod.lua
+++ mod.lua
@@ -209,71 +209,75 @@ function Mod:init()
         Utils.hook(ChaserEnemy, "onCollide", function(orig, self, player)
 
             if self:isActive() and player:includes(Player) then
-                self.encountered = true
-                local encounter
-                local enemy
-            
-                if self.encounter and self.light_encounter then
-                    if Game:isLight() then
-                        encounter = self.light_encounter
-                        enemy = self.light_enemy
-                    else
-                        encounter = self.encounter
-                        enemy = self.enemy
-                    end
-                elseif self.encounter then
-                    encounter = self.encounter
-                    enemy = self.enemy
-                elseif self.light_encounter then
-                    encounter = self.light_encounter
-                    enemy = self.light_enemy
-                end
+				if player.invincible_colors then
+					self:explode()
+				else
+					self.encountered = true
+					local encounter
+					local enemy
 				
-		        Mod.back_attack = false
-		        local angle = Utils.angle(Game.world.player.x, Game.world.player.y, self.x, self.y)
-		        local facing = Utils.facingFromAngle(angle)
-		        if facing == self:getBackFace() and Kristal.getLibConfig("better_battles", "back_attack") then
-		        	print("Back attack!")
-		        	Mod.back_attack = true
-		        end
+					if self.encounter and self.light_encounter then
+						if Game:isLight() then
+							encounter = self.light_encounter
+							enemy = self.light_enemy
+						else
+							encounter = self.encounter
+							enemy = self.enemy
+						end
+					elseif self.encounter then
+						encounter = self.encounter
+						enemy = self.enemy
+					elseif self.light_encounter then
+						encounter = self.light_encounter
+						enemy = self.light_enemy
+					end
+					
+					Mod.back_attack = false
+					local angle = Utils.angle(Game.world.player.x, Game.world.player.y, self.x, self.y)
+					local facing = Utils.facingFromAngle(angle)
+					if facing == self:getBackFace() and Kristal.getLibConfig("better_battles", "back_attack") then
+						print("Back attack!")
+						Mod.back_attack = true
+					end
 
-                if not encounter then
-                    if Game:isLight() and MagicalGlassLib:getLightEnemy(self.enemy or self.actor.id) then
-                        encounter = LightEncounter()
-                        encounter:addEnemy(self.actor.id)
-                    elseif not Game:isLight() and Registry.getEnemy(self.light_enemy or self.actor.id) then
-                        encounter = Encounter()
-                        encounter:addEnemy(self.actor.id)
-                    end
-                end
+					if not encounter then
+						if Game:isLight() and MagicalGlassLib:getLightEnemy(self.enemy or self.actor.id) then
+							encounter = LightEncounter()
+							encounter:addEnemy(self.actor.id)
+						elseif not Game:isLight() and Registry.getEnemy(self.light_enemy or self.actor.id) then
+							encounter = Encounter()
+							encounter:addEnemy(self.actor.id)
+						end
+					end
 
-                if encounter then
-                    self.world.encountering_enemy = true
-			        if not Mod.back_attack then
-			        	self.sprite:setAnimation("hurt")
-			        end
-                    self.sprite.aura = false
-                    Game.lock_movement = true
-                    self.world.timer:script(function(wait)
-				        if Mod.back_attack then
-				        	Assets.playSound("back_attack")
-				        	wait(15/30)
-				        else
-				        	Assets.playSound("tensionhorn")
-				        	wait(8/30)
-				        	local src = Assets.playSound("tensionhorn")
-				        	src:setPitch(1.1)
-				        	wait(12/30)
-				        end
-                        self.world.encountering_enemy = false
-                        Game.lock_movement = false
-                        local enemy_target = self
-                        if enemy then
-                            enemy_target = {{enemy, self}}
-                        end
-                        Game:encounter(encounter, true, enemy_target, self)
-                    end)
-                end
+					if encounter then
+						self.world.encountering_enemy = true
+						if not Mod.back_attack then
+							self.sprite:setAnimation("hurt")
+						end
+						self.sprite.aura = false
+						Game.lock_movement = true
+						self.world.timer:script(function(wait)
+							if Mod.back_attack then
+								Assets.playSound("back_attack")
+								wait(15/30)
+							else
+								Assets.playSound("tensionhorn")
+								wait(8/30)
+								local src = Assets.playSound("tensionhorn")
+								src:setPitch(1.1)
+								wait(12/30)
+							end
+							self.world.encountering_enemy = false
+							Game.lock_movement = false
+							local enemy_target = self
+							if enemy then
+								enemy_target = {{enemy, self}}
+							end
+							Game:encounter(encounter, true, enemy_target, self)
+						end)
+					end
+				end
 
             end
         end)
