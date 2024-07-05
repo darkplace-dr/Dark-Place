e3a58722053c8c71bbb7bfe31c2008b7f77c7f88
Elioze <117308027+Elioze@users.noreply.github.com>
2024-07-05 18:50:27 +0200

    Update focus.lua

--- scripts/data/items/armors/focus.lua
+++ scripts/data/items/armors/focus.lua
@@ -59,12 +59,7 @@ end
 
 -- Function overrides go here
 function item:getDescription() 
-    print(self.id)
-    if self.id == "focus" then
-        return "Hold [color:yellow][" .. string.upper(Input.key_bindings["focus_placebo"][1]) .. "][color:reset] during battle to slow down time.\nTP bar determines amount of time you can use it."
-    else
-        return self.description
-    end
+    return "Hold [color:yellow][" .. string.upper(Input.key_bindings["focus_placebo"][1]) .. "][color:reset] during battle to slow down time.\nTP bar determines amount of time you can use it."
 end
 
 return item
