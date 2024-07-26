5ccf6167e8c2437a5d3bd1e951d5fdb1f7f0ea39
THatWAGuy <milo.skewes@outlook.com>
2024-07-26 00:09:22 -0500

    Add files via upload

---
+++ scripts/world/tilesets/gallery2.lua
@@ -0,0 +1,31 @@
+return {
+  version = "1.10",
+  luaversion = "5.1",
+  tiledversion = "1.11.0",
+  name = "gallery2",
+  class = "",
+  tilewidth = 20,
+  tileheight = 20,
+  spacing = 0,
+  margin = 0,
+  columns = 8,
+  image = "../../../assets/sprites/tilesets/gallery2.png",
+  imagewidth = 160,
+  imageheight = 40,
+  objectalignment = "unspecified",
+  tilerendersize = "grid",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 20,
+    height = 20
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 16,
+  tiles = {}
+}
