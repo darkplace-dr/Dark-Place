63b48da819a15ea3ec3bed6bfd176a7b270a78de
Liu Wenyuan <15816141883@163.com>
2024-07-27 07:32:44 +0800

    wevents/darkdoor: Delegate to correct sprite in ribbit set

--- scripts/world/events/darkdoor.lua
+++ scripts/world/events/darkdoor.lua
@@ -6,7 +6,7 @@ function DarkDoor:init(data)
     super.init(self, data.center_x, data.center_y, data.width, data.height)
 
     self:setOrigin(0.5, 0.5)
-    self:setSprite("world/events/darkdoor_closed")
+    self:setSprite("world/events/hometown_ribbit/closet_door_closed")
 end
 
 return DarkDoor
