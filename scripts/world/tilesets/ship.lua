b56245e07ed96b29356004692ef4c1e407163855
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2024-08-12 23:57:28 -0400

    Earning my sea legs

---
+++ scripts/world/tilesets/ship.lua
@@ -0,0 +1,41 @@
+return {
+  version = "1.10",
+  luaversion = "5.1",
+  tiledversion = "1.10.1",
+  name = "ship",
+  class = "",
+  tilewidth = 107,
+  tileheight = 293,
+  spacing = 0,
+  margin = 0,
+  columns = 0,
+  objectalignment = "unspecified",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 1,
+    height = 1
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 2,
+  tiles = {
+    {
+      id = 0,
+      image = "../../../assets/sprites/world/maps/ship/mast.png",
+      width = 107,
+      height = 293
+    },
+    {
+      id = 1,
+      image = "../../../assets/sprites/world/maps/ship/steering.png",
+      width = 24,
+      height = 42
+    }
+  }
+}
---
+++ scripts/world/tilesets/ship.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="ship" tilewidth="107" tileheight="293" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="107" height="293" source="../../../assets/sprites/world/maps/ship/mast.png"/>
+ </tile>
+ <tile id="1">
+  <image width="24" height="42" source="../../../assets/sprites/world/maps/ship/steering.png"/>
+ </tile>
+</tileset>
