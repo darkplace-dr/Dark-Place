12fa81ac84ac46a1cf020643363f24a53f582859
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2024-08-16 00:22:43 -0400

    Yarr!!

--- scripts/world/maps/marcyquest/ship/deck/map.lua
+++ scripts/world/maps/marcyquest/ship/deck/map.lua
@@ -8,6 +8,23 @@ function Ship:onEnter()
 		Game.world.player:setActor("marcy")
 		Game.world.disable_dark_menu = true
 	end
+	if not Game:getFlag("marcyquest_party_1") then
+		if #Game.party >= 2 then
+			local party1 = NPC(Game.party[2]:getActor().id, 1640, 640, {cutscene="marcyquest.party_1", facing="left", turn=true})
+			Game.world:spawnObject(party1)
+			party1.partyslot = 2
+			if #Game.party >= 3 then
+				local party2 = NPC(Game.party[3]:getActor().id, 1802, 500, {cutscene="marcyquest.party_1", facing="up", turn=true})
+				Game.world:spawnObject(party2)
+				party2.partyslot = 3
+				if #Game.party >= 4 then
+					local party3 = NPC(Game.party[4]:getActor().id, 1360, 940, {cutscene="marcyquest.party_1", facing="down", turn=true})
+					Game.world:spawnObject(party3)
+					party3.partyslot = 4
+				end
+			end
+		end
+	end
 end
 
 return Ship
