d3df45b3d915d7962b9886e7f8466f12767d5619
Simbel0 <simbel.user@gmail.com>
2024-08-19 15:28:29 +0200

    Fix SavePoint

--- scripts/objects/world/events/SavePoint.lua
+++ scripts/objects/world/events/SavePoint.lua
@@ -5,12 +5,13 @@ local SavePoint, super = Class("Savepoint", true)
 function SavePoint:init(x, y, properties)
 	super:init(self, x, y, properties)
 
+	self.override_power = properties["override_power"]
+end
 
-	if Game:getFlag("weird") and (self.text and #self.text > 0) and not properties["override_power"] then
-		print("hey")
+function SavePoint:onInteract()
+	if Game:getFlag("weird") and (self.text and #self.text > 0) and not self.override_power then
 		local party_text = ""
 		for i,member in ipairs(Game.party) do
-			print(i, member.name, #Game.party)
 			party_text = party_text .. member.name
 			if i < #Game.party-1 then
 				party_text = party_text ..",[wait:2] "
@@ -21,6 +22,7 @@ function SavePoint:init(x, y, properties)
 		local verb = #Game.party == 1 and "is" or "are"
 		self.text = {"* " .. party_text .. " "..verb.." filled with power."}
 	end
+	super:onInteract(self)
 end
 
 return SavePoint
