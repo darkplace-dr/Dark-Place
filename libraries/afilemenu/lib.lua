9ee987ce5e60cc07fcf990575f7fa88193b7450f
hyperboid <23138363+polypoyo@users.noreply.github.com>
2024-12-15 01:35:53 -0500

    Enable hookOverlay

--- libraries/afilemenu/lib.lua
+++ libraries/afilemenu/lib.lua
@@ -1,5 +1,61 @@
 local lib = {}
 
+function lib:init()
+    if Kristal.getLibConfig("afilemenu", "hookOverlay") then
+        Utils.hook(Kristal.Overlay, "update", function (orig, self)
+            local do_orig = true
+            if love.keyboard.isDown("escape") and not self.quit_release then
+                if Kristal.Config and Kristal.Config["instantQuit"] then
+                    if Mod ~= nil then
+                        self.quit_release = true
+                        if Kristal.getModOption("hardReset") then
+                            love.event.quit("restart")
+                        else
+                            Kristal.returnToMenu()
+                        end
+                    else
+                        love.event.quit()
+                    end
+                else
+                    if self.quit_alpha < 1 then
+                        self.quit_alpha = math.min(1, self.quit_alpha + DT / 0.75)
+                    end
+                    self.quit_timer = self.quit_timer + DT
+                    if self.quit_timer > 1.2 then
+                        if Game.world and Game.world.map and Game.world.map.id == "fileselect" then
+                            if Kristal.getModOption("hardReset") then
+                                love.event.quit("restart")
+                            else
+                                Kristal.returnToMenu()
+                            end
+                        else
+                            Game:load(nil,nil,true)
+                        end
+                        self.quit_timer = 0
+                        self.quit_alpha = 0
+                        self.quit_release = true
+                        do_orig = false
+                    else
+                        self.quit_timer = self.quit_timer - DT
+                    end
+                end
+            else
+                self.quit_timer = 0
+                if self.quit_alpha > 0 then
+                    self.quit_alpha = math.max(0, self.quit_alpha - DT / 0.25)
+                end
+            end
+    
+            if self.quit_release and not love.keyboard.isDown("escape") then
+                self.quit_release = false
+            end
+            if do_orig then
+                orig(self)
+            end
+        end)
+    end
+end
+
 function lib:getPaletteColor(key)
     local style = "normal"
     if Game.world and Game.world.map then
@@ -32,10 +88,6 @@ function lib:getPaletteColor(key)
     end
     MUSIC_PITCHES.menu = 0.95
     MUSIC_PITCHES.mod_menu = 0.95
-end
-
-function lib:postInit()
-    Game.world:loadMap("fileselect")
 end
 
 return lib
